a99a090c7d9ed1e53a1a9f441e34bdb1257fba7c
jonas <jonas@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2006-09-20 22:40:37 +0000

    *** empty log message ***

---
+++ mgllib/mglGetSecs.m
@@ -0,0 +1,17 @@
+% mglGetSecs: Get current or elapsed time
+%
+%     Syntax: mglGetSecs([t])
+%         By: Jonas Larsson
+%       Date: 04/10/06
+%    Purpose: Timing events
+%      Usage: Get current time in s 
+%             t=mglGetSecs
+% 
+%             Get elapsed time since t0
+%             t=mglGetSecs(t0)
+%
+%
+
+% wrapper for mglGetSecs.mex/glx/mac
+
+  
