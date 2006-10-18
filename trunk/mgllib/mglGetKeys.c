00c3becd5a508cc593b2912ce92c05c6c6948303
jonas <jonas@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2006-10-18 20:41:58 +0000

    X support and bug fixes

--- trunk/mgllib/mglGetKeys.c
+++ trunk/mgllib/mglGetKeys.c
@@ -23,7 +23,6 @@ $Id$
 //////////////
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  int longNum;int bitNum;int logicalNum = 0;
   int returnAllKeys = 1,i,n,displayKey;
   double *inptr,*outptr;
   // get which key we want
@@ -36,11 +35,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     usageError("mglGetKeys");
     return;
   }
+  int verbose = mglGetGlobalDouble("verbose");
 
 #ifdef __APPLE__
+  int longNum;int bitNum;int logicalNum = 0;
   //  get the status of the keyboard
   KeyMap theKeys;
   GetKeys(theKeys);
+  unsigned char *keybytes;
+  short k;
+  keybytes = (unsigned char *) theKeys;
 
   if (!returnAllKeys) {
     // figure out how many elements are desired
@@ -50,43 +54,95 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     outptr = mxGetPr(plhs[0]);
     // now go through and get each key
     for (i=0; i<n; i++) {
-      displayKey = (int)*(inptr+i);
+      displayKey = (int)*(inptr+i)-1; // 1-offset input
       if ((displayKey < 0) || (displayKey > 128)) {
 	mexPrintf("(mglGetKeys) Key %i out of range 1:128",displayKey);
 	return;
       }
-      div_t keypos = div(displayKey-1,32);
-      *(outptr+i) = (theKeys[keypos.quot] >> keypos.rem) & 0x1;
+      k=(short)displayKey;
+      *(outptr+i) = ((keybytes[k>>3] & (1 << (k&7))) != 0);
     }
   }
   else {
     // return it in a logical array
     plhs[0] = mxCreateLogicalMatrix(1,128);
-    mxLogical *outptr = mxGetLogicals(plhs[0]);
+    mxLogical *loutptr = mxGetLogicals(plhs[0]);
    
     // set the elements of the logical array correctly
-    for (longNum = 0;longNum<4;longNum++) {
-      for (bitNum = 0;bitNum<32;bitNum++) {
-	*(outptr+logicalNum++) = (theKeys[longNum] >> bitNum) & 0x1;
+    if (verbose) {
+      mexPrintf("(mglGetKeys) Keystate = ");
+    }
+    for (i=0;i<128;i++) {
+      k=(short)i;
+      *(loutptr+i)=((keybytes[k>>3] & (1 << (k&7))) != 0);
+      if (verbose) {
+	mexPrintf("%i ",(int) *(loutptr+i));
       }
+    }
+    if (verbose) {
+      mexPrintf("\n");
     }
   }
 
-  // and display the same if verbose is set
-  int verbose = mglGetGlobalDouble("verbose");
-  if (verbose) {
-    mexPrintf("(mglGetKeys) Keystate = ");
-    for (longNum=0;longNum<4;longNum++) {
-      for (bitNum = 0;bitNum<32;bitNum++) {
-	mexPrintf("%i ",(theKeys[longNum] >> bitNum) & 0x1);
-      }
-    }
-    mexPrintf("\n");
-  }
+
 #endif
 #ifdef __linux__
-  mexPrintf("(mglGetKeys) Not supported yet on linux\n");
-  return;
+  Display * dpy;
+  int dpyptr=(int)mglGetGlobalDouble("XDisplayPointer");
+  if (dpyptr<=0) {
+    // open a dummy display
+    dpy=XOpenDisplay(0);
+  } else {
+    dpy=(Display *)dpyptr;
+  }
+  char keys_return[32];
+
+  XQueryKeymap(dpy, keys_return);
+  
+  if (!returnAllKeys) {
+    // figure out how many elements are desired
+    n = mxGetN(prhs[0]);
+    // and create an output matrix
+    plhs[0] = mxCreateDoubleMatrix(1,n,mxREAL);
+    outptr = mxGetPr(plhs[0]);
+    // now go through and get each key
+    for (i=0; i<n; i++) {
+      displayKey = (int)*(inptr+i)-1; // input is 1-offset
+      if ((displayKey < 0) || (displayKey > 256)) {
+	mexPrintf("(mglGetKeys) Key %i out of range 1:256",displayKey);
+	return;
+      }
+      int keypos=(int) floor(displayKey/8);
+      int keyshift=displayKey%8;
+
+      *(outptr+i) = (double) (( keys_return[keypos] >> keyshift) & 0x1);
+    }
+  } else {
+    plhs[0] = mxCreateLogicalMatrix(1,256);
+    mxLogical *loutptr = mxGetLogicals(plhs[0]);
+    
+    for (int n=0; n<32; n++) {
+      for (int m=0; m<8; m++) {
+	*(loutptr+n*8+m) = (double) (( keys_return[n] >> m ) & 0x1);
+      }
+    }
+    if (verbose) {
+      mexPrintf("(mglGetKeys) Keystate = ");
+      for (int n=0; n<32; n++) {
+	for (int m=0; m<8; m++) {
+	  mexPrintf("%i ", ( keys_return[n] >> m ) & 0x1 );
+	}
+      }
+      mexPrintf("\n");
+    }
+  }
+
+  if (dpyptr<=0) {
+    XCloseDisplay(dpy);
+  }
+  
+
+
 #endif 
 }
 
