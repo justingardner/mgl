3b00ea91033d6b3fae22b6d58a89a8a778dfdce9
Justin Gardner <JustinGardner@users.noreply.github.com>
2007-10-24 21:36:08 +0000

    Change to warning message when opening AGL windows. Private siwtch display uses a cell array

--- mgllib/mglPrivateMoveWindow.c
+++ mgllib/mglPrivateMoveWindow.c
@@ -5,7 +5,7 @@
           by: Christopher Broussard
         date: 04/03/06
    copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-     purpose: Moves/Resizes the current AGL window.
+     purpose: Moves the current AGL window.
 
 $Id$
 =========================================================================
@@ -22,20 +22,25 @@ $Id$
 //////////////
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	Rect bounds;
-	unsigned int r;
-	WindowRef winRef;
+  Rect bounds;
+  unsigned int r;
+  WindowRef winRef;
 	
-	if (nrhs != 2) {
-		mexErrMsgTxt("(mglPrivateMoveWindow) error: invalid number of args");
-	}
+  if (nrhs != 2) {
+    usageError("mglMoveWindow");
+    return;
+  }
 	
-	bounds.left = (int)mxGetScalar(prhs[0]);
-	bounds.top = (int)mxGetScalar(prhs[1]);
-	r = (unsigned int)mglGetGlobalDouble("windowPointer");
-	winRef = (WindowRef)r;
+  bounds.left = (int)mxGetScalar(prhs[0]);
+  bounds.top = (int)mxGetScalar(prhs[1]);
+  r = (unsigned int)mglGetGlobalDouble("windowPointer");
+  winRef = (WindowRef)r;
 	
-	MoveWindowStructure(winRef, (short)bounds.left, (short)bounds.top);
-	//SetWindowBounds(winRef, kWindowStructureRgn, &bounds);
-	SelectWindow(winRef);
+  if (IsValidWindowPtr(winRef)) {
+    MoveWindowStructure(winRef, (short)bounds.left, (short)bounds.top);
+    SelectWindow(winRef);
+  }
+  else {
+    mexPrintf("(mglPrivateMoveWindow) error: invalid window pointer");
+  }
 }
