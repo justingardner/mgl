9bcd815f2e916b30216085e08a86081ea6c0ef9f
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2008-04-29 22:19:34 +0000

    Function is working now. It returns a print out of code that you have to copy and paste into the matlab buffer to run.

--- trunk/task/recoverStimData.m
+++ trunk/task/recoverStimData.m
@@ -1,15 +1,37 @@
-% check the stack
-[st stackIndex] = dbstack;
+% recoverStimData.m
+%
+%      usage: recoverStimData()
+%         by: justin gardner
+%       date: 04/29/08
+%    purpose: Run this function if you accidently stopped your
+%    stimulus program while it was running (or the program
+%    crashed) and you are at the debugger prompt K>>). This
+%    will spit out some lines of matlab code. Copy these lines
+%    and then paste them back to run them. (It needs to be done
+%    this way since the program has to search up and down the stack
+%    for the correct variables by using dbup/dbdown which can't be
+%    used inside a function). This will find your myscreen and task
+%    variables and then call saveStimData to save
+%    them. Alternatively you can always go look for your myscreen
+%    and task variables yourself and call saveStimData. Just be
+%    sure that you get the full task variable and not just one
+%    part of the task cell array.
+%  
+% 
+%
+%function retval = recoverStimData()
 
-% make sure we are at bottom of stack. Note that we use
-% eval to make this whole string of commands to avoid
+% check the stack
+[st stackIndex] = evalin('caller','dbstack');
+% make sure we are at bottom of stack. 
+% Note that we use eval to make this whole string of commands to avoid
 % a lot of weirdness with code jumping up and down contexts
 evalstr = '';
-for i = 1:stackIndex-1
-  evalstr = sprintf('%sdbdown;',evalstr);
+for i = 1:length(st)-2
+  evalstr = sprintf('%sdbdown\n',evalstr);
 end
-% now go up through stack looking for
-% a myscreen or task variable which we can save
+
+% now look for a myscreen and task
 % basically the further up the stack we are the
 % better off we are since lower parts of stack
 % may have only a part of the task variable. However
@@ -19,33 +41,48 @@ end
 % start with recoveredData set to empty
 assignin('base','recoveredMyscreen',[]);
 assignin('base','recoveredTask',[]);
-for i = 1:length(st)
+for i = 1:length(st)-2
   % if myscreen variable exists then assign it in base context
-  evalstr{i} = sprintf('%sif exist(''myscreen'',''var''),',evalstr{i});
-  evalstr{i} = sprintf('%sassignin(''base'',''recoveredMyscreen'',myscreen);end,',evalstr{i});
+  evalstr = sprintf('%sif exist(''myscreen'',''var''),',evalstr);
+  evalstr = sprintf('%sassignin(''base'',''recoveredMyscreen'',myscreen);end\n',evalstr);
 
   % if task variable exists then assign it in base context
   evalstr = sprintf('%sif exist(''task'',''var''),',evalstr);
-  evalstr = sprintf('%sassignin(''base'',''recoveredTask'',task);end,',evalstr);
+  evalstr = sprintf('%sassignin(''base'',''recoveredTask'',task);end\n',evalstr);
   
   % go up one context
   if i ~= length(st)
-    evalstr = sprintf('%sdbup,',evalstr);
+    evalstr = sprintf('%sdbup\n',evalstr);
   end
 end
 
-eval(evalstr);
-keyboard
-% check to see if we have the correct fields
-if isfield(recoveredData,'task') && isfield(recoveredData,'myscreen')
-  recoveredData.myscreen.saveData = -1;
-  saveStimData(recoveredData.myscreen,recoveredData.task);
-elseif isfield(recoveredData,'task')
-  disp(sprintf('(recoverStimData) Could only get task information. Setting as recoveredTask in local environment'));
-  assignin('caller','recoveredTask',recoveredData.task);
-elseif isfield(recoveredData,'myscreen')
-  disp(sprintf('(recoverStimData) Could only get myscreen information. Setting as recoveredMyscreen in local environment'));
-  assignin('caller','recoveredMyscreen',recoveredData.myscreen);
-else
-  disp(sprintf('(recoverStimData) Could not get myscreen or task information'));
-end
+% now check to see if we have recovered anything and save
+evalstr = sprintf('%sif ~isempty(recoveredTask) && ~isempty(recoveredMyscreen)\n',evalstr);
+evalstr = sprintf('%s  recoveredMyscreen.saveData = -1;\n',evalstr);
+evalstr = sprintf('%s  saveStimData(recoveredMyscreen,recoveredTask);\n',evalstr);
+evalstr = sprintf('%selseif ~isempty(recoveredTask)\n',evalstr);
+evalstr = sprintf('%s  errstr = ''(recoverStimData) Only found task variable.'';\n',evalstr);
+evalstr = sprintf('%s  recoveredMyscreen.datadir = ''.'';\n',evalstr);
+evalstr = sprintf('%s  recoveredMyscreen.saveData = -1;\n',evalstr);
+evalstr = sprintf('%s  recoveredMyscreen.stimulusNames = {};\n',evalstr);
+evalstr = sprintf('%s  disp(errstr);\n',evalstr);
+evalstr = sprintf('%s  saveStimData(recoveredMyscreen,recoveredTask);\n',evalstr);
+evalstr = sprintf('%selseif ~isempty(recoveredMyscreen)\n',evalstr);
+evalstr = sprintf('%s  errstr = ''(recoverStimData) Only found myscreen variable.'';\n',evalstr);
+evalstr = sprintf('%s  recoveredMyscreen.saveData = -1;\n',evalstr);
+evalstr = sprintf('%s  disp(errstr);\n',evalstr);
+evalstr = sprintf('%s  saveStimData(recoveredMyscreen,[]);\n',evalstr);
+evalstr = sprintf('%selse\n',evalstr);
+evalstr = sprintf('%s  errstr = ''(recoverStimData) Could not find myscreen or task variable.'';\n',evalstr);
+evalstr = sprintf('%s  disp(errstr);\n',evalstr);
+evalstr = sprintf('%send',evalstr);
+
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('% Copy the lines of code below and then run them by %');
+disp('% pasting them back into your matlab buffer         %');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp(evalstr);
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('% Copy the lines of code above and then run them by %');
+disp('% pasting them back into your matlab buffer         %');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
