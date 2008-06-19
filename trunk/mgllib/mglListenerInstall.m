2c717db89b2fa89c4bca99c0f727f1ad914cc3ba
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2008-06-19 15:46:49 +0000

    This function can now install a matlab callback as a listener and send events to it. Problem is that it crashes when running testExperiment. I think the problem is due to having matlab run two pieces of code at once. Possibilites would be to try to block matlab from running other code while the listener is being called (no idea if this is possible) or to give up on the callback concept and have mglPrivateListenerInstall.c not call matlab but log events in c-memory and then provide another call that would release those events to matlab -- that way matlab wouldn't be called by a separate thread which appears to be the problem.

--- trunk/mgllib/mglListenerInstall.m
+++ trunk/mgllib/mglListenerInstall.m
@@ -1,7 +1,7 @@
-% mglInstallListener.m
+% mglListenerInstall.m
 %
 %        $Id$
-%      usage: mglInstallListener(listnerName,<eventTypes>,<keyNames>)
+%      usage: mglListenerInstall(listnerName,<eventTypes>,<keyNames>)
 %         by: justin gardner
 %       date: 06/18/08
 %  copyright: (c) 2008 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
@@ -9,18 +9,33 @@
 %             That is, you pass it a name of a function and it sets up that
 %             function gets called every time there is a keyboard or mouse event
 %
-function retval = mglInstallListener(listnerName,eventTypes,keyNames)
+function retval = mglListenerInstall(listenerName,eventTypes,keyNames)
 
 % check arguments
 if ~any(nargin == [1 2 3])
-  help mglInstallListener
+  help mglListenerInstall
   return
 end
 
-bogusEvent.keyCode = 0;
-bogusEvent.timeStamp = mglGetSecs;
-eval(sprintf('%s(bogusEvent)',listnerName));
-pointer1 = mglPrivateInstallListener(listnerName);
+% create an init event and run the function. This insures that the
+% callback will be in memory
+initEvent.type = 'init';
+initEvent.keyCode = 0;
+initEvent.timeStamp = mglGetSecs;
+
+% call the listener with the initEvent
+eval(sprintf('%s(initEvent)',listenerName));
+
+% now install the listener
+%[eventTapPointer runLoopPointer] = mglPrivateListenerInstall(listenerName);
+mglPrivateListenerInstall(listenerName);
+
+% keep the pointers
+%global MGL
+%MGL.listener.eventTapPointer = eventTapPointer;
+%MGL.listener.runLoopPointer = runLoopPointer;
+%MGL.listener.listener = listenerName;
+
 
 
 
