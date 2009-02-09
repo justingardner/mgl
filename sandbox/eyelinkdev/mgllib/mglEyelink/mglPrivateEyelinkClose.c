9cffdc379e14f198e2cb8cf85650fb3051eb9178
dep <dep@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2009-02-09 22:09:10 +0000

    mglEyelink: a set of routines for accessing the Eyelink C API mglPrivate funcitons are used for basic API calls mgl functions are used for combining into useful groups (mgllib only) There will need to be a mglEyeTracker set of funcitons for abstracting away from specific eye trackers There will need to be task based functions for data colleciton/gaze contingent, trial calibration, etc.

---
+++ sandbox/eyelinkdev/mgllib/mglEyelink/mglPrivateEyelinkClose.c
@@ -0,0 +1,39 @@
+#ifdef documentation
+=========================================================================
+program: mglPrivateEyelinkClose.c
+by:      eric dewitt and eli merriam
+date:    02/08/09
+copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
+purpose: mex function to open a connection to an Eyelink tracker and configure
+         it for use with the specificed mgl window
+usage:   mglPrivateEyelinkClose(ipaddress, trackedwindow, displaywindow)
+
+
+=========================================================================
+#endif
+
+/////////////////////////
+//   include section   //
+/////////////////////////
+#include "../mgl.h"
+#include <eyelink.h>
+
+/////////////
+//   main   //
+//////////////
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+
+    if (nrhs!=0) /* What arguments should this take? */
+    {
+        usageError("mglPrivateEyelinkClose");
+        return;
+    }
+
+    close_eyelink_connection();
+    mexPrintf("(mglPrivateEyelinkClose) MGL Eyelink tracker link closed.\n");
+    
+}
+
+
---
+++ sandbox/eyelinkdev/mgllib/mglEyelink/mglPrivateEyelinkOpen.c
@@ -0,0 +1,87 @@
+#ifdef documentation
+=========================================================================
+program: mglPrivateEyelinkOpen.c
+by:      eric dewitt and eli merriam
+date:    02/08/09
+copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
+purpose: mex function to open a connection to an Eyelink tracker and configure
+         it for use with the specificed mgl window
+usage:   mglPrivateEyelinkOpen(ipaddress, trackedwindow, displaywindow)
+
+
+=========================================================================
+#endif
+
+/////////////////////////
+//   include section   //
+/////////////////////////
+#include "../mgl.h"
+#include <eyelink.h>
+
+/////////////
+//   main   //
+//////////////
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+
+    if (nrhs<1 || nrhs>2) /* What arguments should this take? */
+    {
+        usageError("mglPrivateEyelinkOpen");
+        return;
+    }
+
+    /* input must be a string */
+    if ( mxIsChar(prhs[0]) != 1)
+        mexErrMsgTxt("Input must be a string.");
+
+    /* input must be a row vector */
+    if (mxGetM(prhs[0])!=1)
+        mexErrMsgTxt("Input must be a row vector.");    
+        
+    char *trackerip;
+    mwSize buflen;
+
+    /* get the length of the input string */
+    buflen = (mxGetM(prhs[0]) * mxGetN(prhs[0])) + 1;
+
+    /* copy the string data from prhs[0] into a C string input_ buf.    */
+    trackerip = mxArrayToString(prhs[0]);
+
+    if(trackerip == NULL) 
+        mexErrMsgTxt("Could not convert input to string.");
+
+    if (set_eyelink_address(trackerip)==-1){
+        mexErrMsgTxt("Could not parse IP addrss.");     
+    }    
+    
+
+    /* **** TODO: Clean up the trackercontype code to use integer values? */
+    double *trackerconntype = 0;
+    
+    if (nrhs==2) {
+        /* optional parameter which controlls the link connection type */
+        if (mxGetM(prhs[1]) != 1 && mxGetN(prhs[1]) != 1){
+            mexErrMsgTxt("Connection type must be an single value.");
+        } else {
+            /* should be a real data access call */
+            trackerconntype = (double*)mxGetPr(prhs[1]);
+            // if (trackerconntype[0] != -1 || trackerconntype[0] != 0 || trackerconntype[0] != 1) {
+            //     mexErrMsgTxt("Connection type must be one of {-1, 0, 1}.");
+            // }
+        }
+    }
+
+    if(open_eyelink_connection(trackerconntype[0])) {
+        /* abort if we can't open link*/
+        mexErrMsgTxt("Connection failed: could not establish a link.\n");
+    } else {
+        mexPrintf("(mglPrivateEyelinkOpen) MGL Eyelink tracker link established.\n");
+        mexPrintf("(mglPrivateEyelinkOpen) MGL Eyelink tracker IP %s.\n", trackerip);
+        mxFree(trackerip);
+        
+    }
+    
+}
+
+
