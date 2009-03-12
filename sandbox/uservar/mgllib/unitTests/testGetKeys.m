da9b900b90e5d670c3ba2028e7b5b701ecaad54c
dep <dep@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2009-03-12 22:09:53 +0000

    Fixed setup, timing and printouts for testGetKeys

--- sandbox/uservar/mgllib/unitTests/testGetKeys.m
+++ sandbox/uservar/mgllib/unitTests/testGetKeys.m
@@ -1,42 +1,42 @@
-mglOpen()
+mglOpen(0)
+mglListener('init');
+resultN = 1;
+startT = mglGetSecs;
+fprintf(1,'testing mglGetKeys\n');
+while mglGetSecs(startT) < 60
+    mglGetKeysResults(resultN).keys = mglGetKeys;
+    mglGetKeysResults(resultN).time = mglGetSecs;
+    mglGetKeysResults(resultN).requested = 0;
+    resultN = resultN + 1;
+    fprintf(1,'.');
+    pause(1);        
+end
+
+startT = mglGetSecs;
+fprintf(1,'\nPlease press keys during this period.\n');
+while mglGetSecs(startT) < 60
+    mglGetKeysResults(resultN).keys = mglGetKeys;
+    mglGetKeysResults(resultN).time = mglGetSecs;
+    mglGetKeysResults(resultN).requested = 1;
+    resultN = resultN + 1;
+    fprintf(1,'.');
+    pause(1);        
+end
+fprintf(1,'\nFinished collecting data.\n');
+
 
 % resultN = 1;
 % startT = mglGetSecs;
-% fprintf(1,'testing mglGetKeys');
+% fprintf(1,'testing mglGetKeyEvent\n');
 % while mglGetSecs(startT) < 60
-%     mglGetKeysResults(resultN).keys = mglGetKeys;
-%     mglGetKeysResults(resultN).time = mglGetSecs;
-%     mglGetKeysResults(resultN).requested = 0;
+%     mglGetKeyEventResults(resultN).keys = mglGetKeyEvent;
+%     mglGetKeyEventResults(resultN).time = mglGetSecs;
+%     mglGetKeyEventResults(resultN).requested = 0;
 %     resultN = resultN + 1;
 %     fprintf(1,'.');
 %     pause(1);        
 % end
 % 
-% startT = mglGetSecs;
-% fprintf(1,'\nPlease press keys during this period.\n');
-% while mglGetSecs(startT) < 60
-%     mglGetKeysResults(resultN).keys = mglGetKeys;
-%     mglGetKeysResults(resultN).time = mglGetSecs;
-%     mglGetKeysResults(resultN).requested = 1;
-%     resultN = resultN + 1;
-%     fprintf(1,'.');
-%     pause(1);        
-% end
-% fprintf(1,'\nFinished collecting data.\n');
-
-
-resultN = 1;
-startT = mglGetSecs;
-fprintf(1,'testing mglGetKeys');
-while mglGetSecs(startT) < 60
-    mglGetKeyEventResults(resultN).keys = mglGetKeyEvent;
-    mglGetKeyEventResults(resultN).time = mglGetSecs;
-    mglGetKeyEventResults(resultN).requested = 0;
-    resultN = resultN + 1;
-    fprintf(1,'.');
-    % pause(1);        
-end
-
 % startT = mglGetSecs;
 % fprintf(1,'\nPlease press keys during this period.\n');
 % while mglGetSecs(startT) < 60
@@ -48,5 +48,6 @@ end
 %     pause(1);        
 % end
 % fprintf(1,'\nFinished collecting data.\n');
-% 
+
+mglListener('quit');
 
