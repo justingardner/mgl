f872738f21f38dedc772ab8dd08a3fff7c0a7169
Justin Gardner <JustinGardner@users.noreply.github.com>
2009-06-30 14:12:39 +0000

    Removed myscreen.tickScreen -- we are not going to do it this way, since it is annoying to have too many different versions of tickScreen. Instead initScreen will set a bit (useDigIO) which will specify whether to use digIO for reading acquisition and responses

--- utils/mglRetinotopy.m
+++ utils/mglRetinotopy.m
@@ -48,7 +48,7 @@
 function myscreen = mglRetinotopy(varargin)
 
 % evaluate the arguments
-eval(evalargs(varargin,0));
+eval(evalargs(varargin,0,0,{'wedges','rings','wedgesOrRings','direction','dutyCycle','stepsPerCycle','stimulusPeriod','numCycles','doEyeCalib','initialHalfCycle','volumesPerCycle'}));
 
 % setup default arguments
 if exist('wedges','var') && ~isempty(wedges),wedgesOrRings = 1;,end
@@ -155,7 +155,7 @@ while (phaseNum <= length(task{2})) && ~myscreen.userHitEsc
   % update the fixation task
   [task{1} myscreen] = updateTask(task{1},myscreen,1);
   % flip screen
-  myscreen = myscreen.tickScreen(myscreen,task);
+  myscreen = tickScreen(myscreen,task);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -389,7 +389,7 @@ end
 
 % evalargs.m
 %
-%      usage: evalargs(varargin,<alwaysUseNextArg>)
+%      usage: evalargs(varargin,<alwaysUseNextArg>,<verbose>,<validVarnameList>)
 %         by: justin gardner
 %       date: 12/13/05
 %    purpose: passed in varargin, returns a string
@@ -411,20 +411,41 @@ end
 %             will do
 %               var1='str'
 %
-%             when eval is called, this will print out
-%             what the variables are being set to. If
-%             you want it to run quietly, do:
-%             eval(evalargs({'gVerbose=0','var1',...}));
-function evalstr = evalargs(args,alwaysUseNextArg)
+%             when eval is called, you can have it print
+%             out of a list of what is being set by setting the
+%             verbose argument to 1.
+%
+%             validVarnameList is a cell array of names that will
+%             be checked against to see if the variables that are
+%             being set are valid variable names, e.g.
+%
+%             eval(evalargs(varargin,[],[],{'test1','test2'}));
+%             will print out a warning if anything other than test1 and test2 are set
+function evalstr = evalargs(args,alwaysUseNextArg,verbose,validVarnameList)
 
-if ~any(nargin == [1 2])
+if ~any(nargin == [1 2 3 4])
+  evalstr = '';
   help evalargs;
   return
 end
 
-if ~exist('alwaysUseNextArg','var'),alwaysUseNextArg=1;end
+% default to always using next argument
+if ieNotDefined('alwaysUseNextArg'),alwaysUseNextArg=1;end
+if ieNotDefined('verbose'),verbose=0;end
 
-evalstr = 'global gVerbose;oldgVerbose=gVerbose;gVerbose=1;';
+% get function name
+st = dbstack;
+funname = st(end).name;
+
+% only check the variable names if we are given a list of names
+if ieNotDefined('validVarnameList')
+  checkValidVarnameList = 0;
+else
+  checkValidVarnameList = 1;
+end
+
+% start the eval string
+evalstr = '';
 % check arguments in
 skipnext = 0;
 for i = 1:length(args)
@@ -437,7 +458,7 @@ for i = 1:length(args)
   if isstr(args{i}) && ~isempty(strfind(args{i},'='))
     % if the argument is a numeric, than just set it
     if ((exist(args{i}(strfind(args{i},'=')+1:end)) ~= 2) && ...
-	~isempty(mystr2num(args{i}(strfind(args{i},'=')+1:end))))
+	~isempty(mrStr2num(args{i}(strfind(args{i},'=')+1:end))))
       evalstr = sprintf('%s%s;',evalstr,args{i});
     % same for a quoted string
     elseif args{i}(strfind(args{i},'=')+1)==''''
@@ -448,7 +469,15 @@ for i = 1:length(args)
     end
     % any quote needs to be two single quotes
     args{i}(strfind(args{i},''''))='"';
-    evalstr = sprintf('%sif gVerbose,disp(sprintf(''setting: %s''));,end,',evalstr,args{i});
+    % if verbose display setting
+    if verbose
+      evalstr = sprintf('%sdisp(sprintf(''setting: %s''));,',evalstr,args{i});
+    end
+    % check against validVarnameList
+    if checkValidVarnameList && ~any(strcmp(args{i}(1:strfind(args{i},'=')-1),validVarnameList))
+      disp(sprintf('(evalargs) Variable %s for function %s is not known',args{i}(1:strfind(args{i},'=')-1),funname));
+      keyboard
+    end
   % if it is not evaluated then either it means to set the variable
   % or to set the variable to the next argument, we determine this
   % by whether the next argument is a string or not. If it is not
@@ -458,17 +487,40 @@ for i = 1:length(args)
       % set the variable to the next argument
       if ~isstr(args{i+1})
 	evalstr = sprintf('%s%s=varargin{%i};',evalstr,args{i},i+1);
-	evalstr = sprintf('%sif gVerbose,disp(sprintf(''setting: %s=varargin{%i}''));,end,',evalstr,args{i},i+1);
+        % if verbose display setting
+	if verbose
+	  evalstr = sprintf('%sdisp(sprintf(''setting: %s=varargin{%i}''));,',evalstr,args{i},i+1);
+	end
+	if checkValidVarnameList && ~any(strcmp(args{i},validVarnameList))
+	  disp(sprintf('(evalargs) Variable %s for function %s is not known',args{i},funname));
+	  keyboard
+	end
       else
 	evalstr = sprintf('%s%s=''%s'';',evalstr,args{i},args{i+1});
-	evalstr = sprintf('%sif gVerbose,disp(sprintf(''setting: %s=''''%s''''''));,end,',evalstr,args{i},args{i+1});
+        % if verbose display setting
+	if verbose
+	  evalstr = sprintf('%sdisp(sprintf(''setting: %s=''''%s''''''));,',evalstr,args{i},args{i+1});
+	end
+        % check against validVarnameList
+	if checkValidVarnameList && ~any(strcmp(args{i},validVarnameList))
+	  disp(sprintf('(evalargs) Variable %s for function %s is not known',args{i},funname));
+	  keyboard
+	end
       end
       skipnext = 1;
     else
       % just set the variable to one, since the next argument
       % does not contain a non string
       evalstr = sprintf('%s%s=1;',evalstr,args{i});
-      evalstr = sprintf('%sif gVerbose,disp(sprintf(''setting: %s=1''));,end,',evalstr,args{i});
+      % if verbose display setting
+      if verbose
+	evalstr = sprintf('%sdisp(sprintf(''setting: %s=1''));,',evalstr,args{i});
+      end
+      % check against validVarnameList
+      if checkValidVarnameList && ~any(strcmp(args{i},validVarnameList))
+	disp(sprintf('(evalargs) Variable %s for function %s is not known',args{i},funname));
+	keyboard
+      end
     end
   else
     % skip anythin we don't know about
@@ -479,7 +531,7 @@ for i = 1:length(args)
   end
 end
 
-evalstr = sprintf('%sgVerbose=oldgVerbose;',evalstr);
+evalstr = sprintf('%s',evalstr);
 
 
 % mystr2num
