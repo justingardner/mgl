ca52e9180532329b7f547501dbef4ab265f86906
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2009-07-17 13:21:59 +0000

    More changes to make editing screen params work better

--- trunk/task/mglGetScreenParams.m
+++ trunk/task/mglGetScreenParams.m
@@ -6,12 +6,16 @@
 %       date: 07/17/09
 %    purpose: just returns the screenParams
 %
-function screenParams = mglGetScreenParams()
+function screenParams = mglGetScreenParams(convertDigFields)
 
 % check arguments
-if ~any(nargin == [0])
+if ~any(nargin == [0 1])
   help mglGetScreenParams
   return
+end
+
+if nargin == 0
+  convertDigFields = 1;
 end
 
 % get then name of the screen params filename
@@ -42,4 +46,51 @@ elseif ~isempty(screenParams)
 end
 
 if isempty(screenParams),return,end
+
+% make sure that dispalySize and flipHV are row arrays
+rowArrayVars = {'displaySize','flipHV','diginAcqType','diginResponseType'};
+for i = 1:length(screenParams)
+  for j = 1:length(rowArrayVars)
+    if isfield(screenParams{i},rowArrayVars{j})
+      if size(screenParams{i}.(rowArrayVars{j}),1) > 1
+	screenParams{i}.(rowArrayVars{j}) = screenParams{i}.(rowArrayVars{j})';
+      end
+    end
+  end
+end
+
+% set some default values
+defaultValues = {{'diginAcqType',[0 1]},{'diginResponseType',[0 1]}};
+for i = 1:length(screenParams)
+  for j = 1:length(defaultValues)
+    if ~isfield(screenParams{i},defaultValues{j}{1})
+      screenParams{i}.(defaultValues{j}{1}) = defaultValues{j}{2};
+    end
+  end
+end
+
+if convertDigFields
+  % check for digin fields and then convert them to a structure. We are doing this just for convenience
+  % in the saveParams strucutre we save fields like diginAcqLine and in initScreen we want to group
+  % them together into fields like digin.acqLine, so we do a little conversion here
+  for i = 1:length(screenParams)
+    digin = [];
+    paramNames = fieldnames(screenParams{i});
+    for j = 1:length(paramNames)
+      if findstr('digin',paramNames{j}) == 1
+	% get the field that begins with digin and pack into a structure
+	if ~isstruct(screenParams{i}.(paramNames{j}))
+	  paramNames{6} = lower(paramNames{6});
+	  digin.(paramNames{j}(6:end)) = screenParams{i}.(paramNames{j});
+	end
+	% remove the field from screenParams
+	screenParams{i} = rmfield(screenParams{i},paramNames{j});
+      end
+    end
+    if ~isempty(digin)
+      screenParams{i}.digin = digin;
+    end
+  end
+end
+
 
