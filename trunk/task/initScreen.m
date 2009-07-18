bc4bf16f6828189d05c23ab8446b6a74e22bf82c
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2009-07-18 10:20:42 +0000

    Moved getHostaName into its own function, endScreen changed comments

--- trunk/task/initScreen.m
+++ trunk/task/initScreen.m
@@ -28,7 +28,7 @@ myscreen.ver = 1.1;
 
 % get computer name
 if ~isfield(myscreen,'computer')
-  myscreen.computer = getHostName;
+  myscreen.computer = mglGetHostName;
 end
 
 % default monitor gamma (used when there is no calibration file)
@@ -490,51 +490,6 @@ end
 
 % get all pending keyboard events
 mglGetKeyEvent([],1);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% support function that gets host name using system command
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function hostname = getHostName()
-
-[retval hostname] = system('hostname');
-% sometimes there is more than one line (errors from csh startup)
-% so need to strip those off
-hostname = strread(hostname,'%s','delimiter','\n');
-hostname = hostname{end};
-if (retval == 0)
-  % get it again
-  [retval hostname2] = system('hostname');
-  hostname2 = strread(hostname2,'%s','delimiter','\n');
-  hostname2 = hostname2{end};
-  if (retval == 0)
-    % find the matching last characers
-    % this is necessary, because matlab's system command
-    % picks up stray key strokes being written into
-    % the terminal but puts those at the beginning of
-    % what is returned by stysem. so we run the
-    % command twice and find the matching end part of
-    % the string to get the hostrname
-    minlen = min(length(hostname),length(hostname2));
-    for k = 0:minlen
-      if (k < minlen)
-	if hostname(length(hostname)-k) ~= hostname2(length(hostname2)-k)
-	  break
-	end
-      end
-    end
-    if (k > 0)
-      hostname = hostname(length(hostname)-k+1:length(hostname));
-      hostname = lower(hostname);
-      hostname = hostname(find(((hostname <= 'z') & (hostname >= 'a')) | '.'));
-    else
-      hostname = 'unknown';
-    end
-  else
-    hostname = 'unknown';
-  end
-else
-  hostname = 'unknown';
-end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 % check if it is a file
