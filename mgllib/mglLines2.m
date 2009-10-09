a7afa2889485d4a9290279935d7c7feefb86641f
Justin Gardner <JustinGardner@users.noreply.github.com>
2009-10-09 01:25:29 +0000

    Added a check for bad color inputs -- if you somehow passed in a cell array instead of a regular array.

--- mgllib/mglLines2.m
+++ mgllib/mglLines2.m
@@ -1,11 +1,14 @@
 % mglLines2.m
 %
 %        $Id$
-%       usage: mglLines(x0, y0, x1, y1,size,color,bgcolor)
+%       usage: mglLines(x0, y0, x1, y1,size,color)
 %          by: justin gardner
 %        date: 04/03/06
 %  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-%     purpose: mex function to plot lines on an OpenGL screen opened with glopen
+%     purpose: mex function to plot lines on an OpenGL screen opened with mglOpen.
+%              x0 and y0 are starting and x1 and y1 are ending point of line
+%              size is line width and color is an color value (usually an array
+%              of three number between 0 and 1). 
 %       e.g.: 
 %
 %mglOpen
