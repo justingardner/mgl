1a7fb1f842da436b281306b9a88fbe86fa2d933f
dep <dep@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2010-03-16 16:12:45 +0000

    formatting; protection added to callback

--- trunk/task/endTask.m
+++ trunk/task/endTask.m
@@ -9,42 +9,42 @@
 %  copyright: (c) 2006 Justin Gardner (GPL see mgl/COPYING)
 %
 function myscreen = endTask(myscreen,task)
-    
-    % check arguments
-    if ~any(nargin == [2])
-        help endTask
-        return
-    end
-    
-    % quit keyboard listener
-    mglListener('quit');
-    
-    % compute traces and save data
-    myscreen = endScreen(myscreen);
-    % This funciton will check for existing stim files and update the save
-    % number appropriately. This should ensure that the eyelink data will always
-    % matched, as the stim and eyelink will have the same file number.
-    saveStimData(myscreen,task);
-    
-    if myscreen.eyetracker.init
-        feval(myscreen.eyetracker.callback.endTracking,task,myscreen);
-    end
-    
-    
-    mydisp(sprintf('End task\n'));
-    % we are done
-    myscreen.task = task;
-    % package up stimuli
-    myscreen.stimuli = '';
-    for stimulusNum = 1:length(myscreen.stimulusNames)
-        eval(sprintf('global %s;',myscreen.stimulusNames{stimulusNum}));
-        eval(sprintf('myscreen.stimuli{end+1} = %s;',myscreen.stimulusNames{stimulusNum}));
-    end
-    
-    % switch back to current directory
-    if isfield(myscreen,'pwd') && isdir(myscreen.pwd)
-        cd(myscreen.pwd);
-    end
+
+  % check arguments
+  if ~any(nargin == [2])
+    help endTask
+    return
+  end
+
+  % quit keyboard listener
+  mglListener('quit');
+
+  % compute traces and save data
+  myscreen = endScreen(myscreen);
+  % This funciton will check for existing stim files and update the save
+  % number appropriately. This should ensure that the eyelink data will always
+  % matched, as the stim and eyelink will have the same file number.
+  saveStimData(myscreen,task);
+
+  if myscreen.eyetracker.init && isfield(myscreen.eyetracker.callback, 'endTracking')
+    feval(myscreen.eyetracker.callback.endTracking,task,myscreen);
+  end
+
+
+  mydisp(sprintf('End task\n'));
+  % we are done
+  myscreen.task = task;
+  % package up stimuli
+  myscreen.stimuli = '';
+  for stimulusNum = 1:length(myscreen.stimulusNames)
+    eval(sprintf('global %s;',myscreen.stimulusNames{stimulusNum}));
+    eval(sprintf('myscreen.stimuli{end+1} = %s;',myscreen.stimulusNames{stimulusNum}));
+  end
+
+  % switch back to current directory
+  if isfield(myscreen,'pwd') && isdir(myscreen.pwd)
+    cd(myscreen.pwd);
+  end
 
 end
 
