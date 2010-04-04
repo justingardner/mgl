a05758567161199bab35e50e0e96c94b6193a901
Justin Gardner <JustinGardner@users.noreply.github.com>
2010-04-04 14:09:56 +0000

    Adding ability to post mouse events

--- mgllib/mglPostEvent.m
+++ mgllib/mglPostEvent.m
@@ -15,16 +15,22 @@
 %             If the time is a positive number it is assumed to be 
 %             a system time (in seconds) like one returned by mglGetSecs.
 %
+%             To move the mouse to a certain screen position.
+%             mglPostEvent('mousemove',-2,100,300);
+%
+%             To click the mouse at a certain screen position.
+%             mglPostEvent('mousepress',-2,'left',100,300);
+%
 %             To list all pending events:
 %             mglPostEvent('list');
 %
 %             To shut down the thread that dispatches the events (which
 %             is started either with 'init', or simply by posting an event):
 %             mglPostEvent('quit');
-function mglPostEvent(command,time,arg1)
+function mglPostEvent(command,time,arg1,arg2,arg3)
 
 % check arguments
-if ~any(nargin == [1 3])
+if ~any(nargin == [1 3 4 5])
   help mglPostEvent;
   return
 end
@@ -63,6 +69,32 @@ elseif strcmp(lower(command),'keypress')
   for i = 1:length(keyCode)
     mglPrivatePostEvent(2,time,keyCode(i),1);
     mglPrivatePostEvent(2,time+0.1,keyCode(i),0);
+    time = time+0.01;
+  end
+elseif strcmp(lower(command),'mousemove')
+  % if post event is not enabled, enable it
+  if isempty(mglGetParam('postEventEnabled')) || (mglGetParam('postEventEnabled') ~= 1)
+    disp(sprintf('(mglPostEvent) Enabling post event thread.'));
+    mglPrivatePostEvent(1);
+  end
+  if nargin < 4,help mglPostEvent,return,end
+  % and post the event. 
+  mglPrivatePostEvent(4,time,arg1,arg2)
+elseif strcmp(lower(command),'mousepress')
+  % if post event is not enabled, enable it
+  if isempty(mglGetParam('postEventEnabled')) || (mglGetParam('postEventEnabled') ~= 1)
+    disp(sprintf('(mglPostEvent) Enabling post event thread.'));
+    mglPrivatePostEvent(1);
+  end
+  if nargin < 5,help mglPostEvent,return,end
+  % and post the event. 
+  if strcmp(arg1,'left')
+    mglPrivatePostEvent(5,time,0,arg2,arg3);
+    mglPrivatePostEvent(6,time+0.1,0,arg2,arg3);
+    time = time+0.01;
+  else
+    mglPrivatePostEvent(5,time,1,arg2,arg3);
+    mglPrivatePostEvent(6,time+0.1,1,arg2,arg3);
     time = time+0.01;
   end
 else
