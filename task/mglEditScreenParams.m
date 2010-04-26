ac8aceec2f959f66ad99d9c5e1e77332da4c6a01
Justin Gardner <JustinGardner@users.noreply.github.com>
2010-04-26 05:44:34 +0000

    mglEditScreenParams  now calls mglDefaultScreenParams

--- task/mglEditScreenParams.m
+++ task/mglEditScreenParams.m
@@ -47,7 +47,7 @@ end
 % get the screenParams
 screenParams = mglGetScreenParams;
 if isempty(screenParams)
-  screenParams{1} = defaultScreenParams;
+  screenParams{1} = mglDefaultScreenParams;
 end
 
 % get the hostname
@@ -78,7 +78,7 @@ if isempty(params),return,end
 
 % add this display if asked for
 if isequal(params.addDisplay,'add')
-  screenParams{end+1} = defaultScreenParams;
+  screenParams{end+1} = mglDefaultScreenParams;
 end
 
 % delete computers list
@@ -91,7 +91,7 @@ end
 % revert displays
 revertDisplay = str2num(params.revertDisplay);
 for i = 1:length(revertDisplay)
-  screenParams{revertDisplay(i)} = defaultScreenParams(screenParams{revertDisplay(i)});
+  screenParams{revertDisplay(i)} = mglDefaultScreenParams(screenParams{revertDisplay(i)});
 end
 
 % get parameters for chosen computer
@@ -584,58 +584,6 @@ function retval = revertDisplay(params)
 
 retval = sprintf('%s %i',params.revertDisplay,params.computerNum);
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%   defaultScreenParams   %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function screenParams = defaultScreenParams(screenParams)
-
-% get host/display name
-if nargin < 1
-  screenParams.computerName = mglGetHostName;
-  screenParams.displayName = '';
-end
-
-displays = mglDescribeDisplays;
-
-% get screen settings
-params.useCustomScreenSettings = 0;
-screenParams.screenNumber = length(displays);
-screenParams.screenWidth = displays(end).screenSizePixel(1);
-screenParams.screenHeight = displays(end).screenSizePixel(2);
-screenParams.framesPerSecond = displays(end).refreshRate;
-
-% get display settings
-screenParams.displayDistance = 57;
-screenParams.displaySize = [50.8 38.1];
-screenParams.displayPos = [0 0];
-screenParams.flipHV = [0 0];
-screenParams.hideCursor = 0;
-screenParams.autoCloseScreen = 1;
-
-% get file saving settings
-screenParams.saveData = 0;
-
-% keyboard settings
-screenParams.eatKeys = 0;
-screenParams.backtickChar = '`';
-screenParams.responseKeys = {'1' '2' '3' '4' '5' '6' '7' '8' '9' '0'};
-
-% calibration info
-screenParams.calibType = 'None';
-screenParams.calibFilename = '';
-screenParams.monitorGamma = [];
-
-% digio
-screenParams.digin.use = 0;
-screenParams.digin.portNum = 2;
-screenParams.digin.acqLine = 0;
-screenParams.digin.acqType = 1;
-screenParams.digin.responseLine = [1 2 3 4 5 6 7];
-screenParams.digin.responseType = 1;
-
-% eyeTracker
-screenParams.eyeTrackerType = 'None';
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%   params2Str2num   %%
