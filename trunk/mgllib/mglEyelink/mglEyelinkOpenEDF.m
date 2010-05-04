f73fac6b79355194651163353cf8394fdc6ab8e4
dep <dep@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2010-05-04 17:03:05 +0000

    non-private wrapper for opening EDF files

---
+++ trunk/mgllib/mglEyelink/mglEyelinkOpenEDF.m
@@ -0,0 +1,18 @@
+function  = mglEyelinkOpenEDF(filename)
+%   MGLEYELINKOPENEDF   Opens an edf file on the Eyelink computer for writing
+%     [] = MGLEYELINKOPENEDF(FILENAME)
+% 
+%   Opens a file on the Eyelink computer for writing data. The filename must
+%   be a valid DOS filename
+%    
+%   Created by Eric DeWitt on 2010-03-25.
+%   Copyright (c)  Eric DeWitt. All rights reserved.
+
+  % check for filename valididty. This could be more robust
+  if numel(filename) > 8
+    fprintf(2,'(mglEyelinkOpenEDF) Invalid DOS filename. Filename must be under 8char.');
+    return;
+  end
+  mglPrivateEyelinkOpenEDF(filename);
+
+end %  function
