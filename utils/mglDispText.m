8e052616747559eac2e455b86629dcd3890fa66a
Justin Gardner <JustinGardner@users.noreply.github.com>
2010-06-28 07:12:35 +0000

    Allows for default text to be shown

--- utils/mglDispText.m
+++ utils/mglDispText.m
@@ -1,21 +1,22 @@
 % mglDispText.m
 %
-%      usage: mglDispText(whichScreen)
+%      usage: mglDispText(<whichScreen>,<defaultText>)
 %         by: justin gardner
 %       date: 05/16/06
 %    purpose: display text to screen (useful for communicating
 %             w/subject). 
 % 
 %
-function retval = mglDispText(whichScreen)
+function mglDispText(whichScreen,defaultText)
 
 % check arguments
-if ~any(nargin == [0 1])
+if ~any(nargin == [0 1 2])
   help disptext
   return
 end
 
 if nargin == 0,whichScreen = [];end
+if nargin < 2,defaultText = [];end
   
 % open the screen
 msc = initScreen(whichScreen);
@@ -29,27 +30,46 @@ if (exist('vFlip','var')==1) & (vFlip == 1),mglVFlip;,end
 abcTexture = mglText(['A':'Z' 'a':'z']);
 textHeight = abcTexture.imageHeight;
 textHeightDevice = textHeight*mglGetParam('yPixelsToDevice');
+textWidth = abcTexture.imageWidth;
+textWidthDevice = textWidth*mglGetParam('xPixelsToDevice');
+deviceRect = mglGetParam('deviceRect');
 
-% get the maximum number of lines we can display
+% get the maximum number of lines we can display and max number of characters
 maxlines = floor(mglGetParam('screenHeight')/textHeight)-1;
+maxStrLen = floor((deviceRect(3)-deviceRect(1))/(textWidthDevice/52));
+
+% default text
+if ~isempty(defaultText)
+  defaultTextTex = mglText(defaultText);
+  mglBltTexture(defaultTextTex,[deviceRect(1) deviceRect(4)-textHeightDevice],-1,1);
+  mglLines2(deviceRect(1),deviceRect(4)-textHeightDevice,deviceRect(3),deviceRect(4)-textHeightDevice,2,[1 1 1 ]);
+%  mglLines2(deviceRect(1),deviceRect(4)-2*textHeightDevice,deviceRect(3),deviceRect(4)-2*textHeightDevice,2,[1 1 1 ]);
+  mglFlush;
+end
 
 % some init values
 str = 'start';textnum = 1;
 
-maxStrLen = 20;minDist = 4;
+minDist = 4;
 % loop to display text
 while 1
   % ask the user what they want to display
   str = input('Text to display (type end to end, type clear to clear screen): ','s');
   % clear the screen
   if strcmp(str,'clear')
-    for i = 1:length(textTexture)
-      mglDeleteTexture(textTexture(i));
+    if exist('textTexture')
+      for i = 1:length(textTexture)
+	mglDeleteTexture(textTexture(i));
+      end
+      clear textTexture;
     end
-    clear textTexture;
     textnum = 1;
     mglClearScreen;
-  % or draw some more text
+    if ~isempty(defaultText)
+      mglBltTexture(defaultTextTex,[deviceRect(1) deviceRect(4)-textHeightDevice],-1,1);
+      mglLines2(deviceRect(1),deviceRect(4)-textHeightDevice,deviceRect(3),deviceRect(4)-textHeightDevice,2,[1 1 1 ]);
+    end
+    % or draw some more text
   elseif strcmp(str,'end')
     break;
   else
@@ -82,14 +102,20 @@ while 1
       % get the next line of text
       textnum = mod(textnum,maxlines)+1;
     end
-    % set vertical offset
-    voffset = textHeightDevice;
     % clear screen
     mglClearScreen;
-    deviceRect = mglGetParam('deviceRect');
+    if isempty(defaultText)
+      % set vertical offset
+      voffset = textHeightDevice;
+    else
+      % draw default text
+      mglBltTexture(defaultTextTex,[deviceRect(1) deviceRect(4)-textHeightDevice],-1,1);
+      mglLines2(deviceRect(1),deviceRect(4)-textHeightDevice,deviceRect(3),deviceRect(4)-textHeightDevice,2,[1 1 1 ]);
+      voffset = textHeightDevice*2;
+    end
     % now go through and draw all text lines in memory
     for i = 1:min(length(textTexture),maxlines)
-      mglBltTexture(textTexture(i),[deviceRect(1) deviceRect(4)-voffset],-1,-1);
+      mglBltTexture(textTexture(i),[deviceRect(1) deviceRect(4)-voffset],-1,1);
       voffset = voffset+textHeightDevice;
     end
   end
