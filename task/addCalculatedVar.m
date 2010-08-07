5217b6077c513a98a3a3a94fbb36e163d269aa8d
Justin Gardner <JustinGardner@users.noreply.github.com>
2010-08-07 09:28:04 +0000

    Function to add a calculated var to a stimfile after the experiment has run

---
+++ task/addCalculatedVar.m
@@ -0,0 +1,86 @@
+% addCalculatedVar.m
+%
+%      usage: addCalculatedVar(varname,varval,stimfileName)
+%         by: justin gardner
+%       date: 08/06/10
+%    purpose: add a computed variable to a stimfile
+% 
+%       e.g.: addCalculatedVar('newvar',[1 3 4 5 6 2 8 10],'100728_stim01');
+%             To specify taskNum (default=1) and phaseNum (default=1)
+%             addCalculatedVar('newvar',[1 3 4 5 6 2 8 10','100728_stim01','taskNum=2','phaseNum=2');
+%   
+%
+function retval = addCalculatedVar(varname,varval,stimfile,varargin)
+
+% check arguments
+if any(nargin < 3)
+  help addCalculatedVar
+  return
+end
+
+taskNum=[];
+phaseNum=[];
+force=[];
+getArgs(varargin,{'taskNum=1','phaseNum=1','force=0'});
+
+stimfile = setext(stimfile,'mat');
+if ~isfile(stimfile)
+  disp(sprintf('(addCalculatedVar) Could not find stimfile %s',stimfile));
+  return
+end
+
+% load and valideate
+s = load(stimfile);
+if ~isfield(s,'myscreen') || ~isfield(s,'task')
+  disp(sprintf('(addCalculatedVar) File %s is not a stimfile - missing myscreen or task',stimfile));
+  return
+end
+
+% make sure task is a cell array
+s.task = cellArray(s.task,2);
+
+% check whether variable already exist
+if any(strcmp(varname,s.task{taskNum}{phaseNum}.randVars.names_))
+  if force
+    disp(sprintf('(addCalculatedVar) Variable %s already exists in task. Overwriting (this will still keep an original copy of stimfile)',varname));
+  else
+    if ~askuser(sprintf('(addCalculatedVar) Variable %s already exists in task. Overwrite existing value (this will still keep an original copy of stimfile)',varname))
+      return
+    end
+  end
+end
+
+% get number of trials
+e = getTaskParameters(s.myscreen,s.task);
+nTrials = e{taskNum}(phaseNum).nTrials;
+
+% check number of trials
+if length(varval) ~= nTrials
+  if force
+    disp(sprintf('(addComuptedVar) Passed in variable values has %i trials, but structure in %s has %i trials',length(varval),stimfile,nTrials));
+  else
+    if ~askuser(sprintf('(addComuptedVar) Passed in variable values has %i trials, but structure in %s has %i trials, continue?',length(varval),stimfile,nTrials))
+      return
+    end
+  end
+end
+
+% now make sure there is an original backup
+originalBackup = sprintf('%s_original.mat',stripext(stimfile));
+if isfile(originalBackup)
+  disp(sprintf('(addComputerBackup) Original backup already exists'));
+else
+  eval(sprintf('save %s -struct s',originalBackup));
+end
+
+% set the new variable
+s.task{taskNum}{phaseNum}.randVars.n_ = s.task{taskNum}{phaseNum}.randVars.n_+1;
+s.task{taskNum}{phaseNum}.randVars.names_{end+1} = varname;
+s.task{taskNum}{phaseNum}.randVars.varlen_(end+1) = length(varval);
+s.task{taskNum}{phaseNum}.randVars.(varname) = varval;
+
+% and save
+disp(sprintf('(addCalculatedVar) Saving variable %s into %s for taskNum=%i phaseNum=%i',varname,stimfile,taskNum,phaseNum));
+eval(sprintf('save %s -struct s',stimfile));
+
+
