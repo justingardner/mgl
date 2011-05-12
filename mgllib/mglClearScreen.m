13f5e1961bccdcae2a86f0122712f933d1609670
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-05-12 18:44:23 +0000

    - Added a new optional argument to mglClearScreen that lets you specify which OpenGL buffers are cleared.  If not passed it defaults to the color buffer as it was before. - Compiled the under 2010b for 64-bit and 2009b for 32-bit.
    	Chris

--- mgllib/mglClearScreen.m
+++ mgllib/mglClearScreen.m
@@ -1,11 +1,17 @@
 % mglClearScreen.m
 %
 %        $Id$
-%      usage: mglClearScreen()
+%      usage: mglClearScreen([clearColor], [clearBits])
 %         by: Jonas Larsson
 %       date: 04/10/06
 %  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-%    purpose: sets the background color
+%    purpose: Sets the background color and clears the buffer.  'clearBits'
+%             is an optional parameter that lets you specify which buffers
+%             are cleared.  The buffers are specifed by a 1x4 array of 1's
+%             and 0's which toggle the buffer bits, where the bits are
+%             [color buffer, depth buffer, accum buffer, stencil buffer].
+%             By default the color buffer is cleared if 'clearBits' isn't
+%             specified.
 %      usage: sets to whatever previous background color was set
 %             mglClearScreen()     
 %
@@ -15,9 +21,12 @@
 %             set to the given [r g b]
 %             mglClearScreen([r g b]) 
 %  
+%             set the clear color and clear the color and depth buffers.
+%             mglClearScreen([r g b], [1 1 0 0]);
 %       e.g.: 
 %
-%mglOpen();
-%mglClearScreen([0.7 0.2 0.5]);
-%mglFlush();
+% mglOpen();
+% mglClearScreen([0.7 0.2 0.5]);
+% mglFlush();
+%
 
