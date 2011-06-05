715d6a340e7fb8026fcdb0edd601c061501731cd
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-06-05 16:52:54 +0000

    - Added some basic shader functionality.  Needs to be expanded.
        CGB

---
+++ mgllib/mglShader.m
@@ -0,0 +1,10 @@
+% mglShader.m
+%
+%        $Id: mglShader.m,v 1.4 2007/01/17 15:49:49 justin Exp $
+%      usage: mglShader('command', [args])
+%         by: Christopher Broussard
+%       date: 11/13/07
+%  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
+%    purpose: Performs various shader functions.
+%      usage:
+%             In
