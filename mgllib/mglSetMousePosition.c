5675cc2ea4361ec6cc426c6ae2a24be5435548fd
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-06-30 16:02:32 +0000

    Changed mglSetMousePosition to use the same coordinate system as that used by mglGetMouse.  A 3rd parameter has now been added that lets you specify which screen the coordinates are relative to.  Compiled under 2010b for 64bit.
    	CGB

--- mgllib/mglSetMousePosition.c
+++ mgllib/mglSetMousePosition.c
@@ -5,10 +5,19 @@
         date: 02/03/11
    copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
        purpose: Sets the position of the mouse cursor.  The position is in
-                absolute screen pixel coordinates.
-       usage: mglSetMousePosition(xPos, yPos)
+                absolute screen pixel coordinates where (0,0) is the  
+				is bottom left corner of the screen.  If targetScreen
+				specified, then the coordinates are relative to that screen.
+				Otherwise, coordinates are relative to the main screen.
+       usage: mglSetMousePosition(xPos, yPos, targetScreen)
+	     
+		      % Move the mouse on the main screen.
+			  mglSetMousePosition(512, 512);
 
-$Id: mglGetMouse.c 894 2011-02-02 18:44:20Z chrg $
+			  % Move the mouse on the secondary screen.
+			  mglSetMousePosition(512, 512, 2);
+
+$Id: mglSetMousePosition.c 894 2011-02-02 18:44:20Z chrg $
 =========================================================================
 #endif
 
@@ -17,17 +26,29 @@ $Id: mglGetMouse.c 894 2011-02-02 18:44:20Z chrg $
 /////////////////////////
 #include "mgl.h"
 
+////////////////////////
+//   define section   //
+////////////////////////
+#define kMaxDisplays 8
+
 /////////////
 //   main   //
 //////////////
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	int xPos, yPos, i;
+	float xPos, yPos;
+	int i;
+	size_t targetScreen = 0;
 	
 	// Check the number of input arguments.
-	if (nrhs != 2) {
+	if (nrhs < 2 || nrhs > 3) {
 		usageError("mglSetMousePosition");
 		return;
+	}
+
+	// Grab the target screen if specified.
+	if (nrhs == 3) {
+		targetScreen = (size_t)mxGetScalar(prhs[2]) - 1;
 	}
 	
 	// Make sure that both inputs are scalars.  Also make sure that passed 
@@ -38,31 +59,47 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 			return;
 		}
 		
-		xPos = (int)mxGetScalar(prhs[0]);
-		if (xPos < 0) {
-			mexPrintf("(mglSetMousePosition) X coordinate must be >= 0.\n");
-			return;
-		}
-		
-		yPos = (int)mxGetScalar(prhs[1]);
-		if (yPos < 0) {
-			mexPrintf("(mglSetMousePosition) Y coordinate must be >= 0.\n");
-			return;
-		}
+		// Grab the desired cursor coordinates.
+		xPos = (float)mxGetScalar(prhs[0]);
+		yPos = (float)mxGetScalar(prhs[1]);
 	}
 	
 #ifdef __APPLE__
+	size_t pxHeight;
 	CGPoint cursorPos;
-	CGError errorCode;
+	CGError errorCode, displayErrorNum;
+	uint32_t numDisplays;
+	CGDirectDisplayID displays[kMaxDisplays];
+
+	// Get a list of displays.
+	displayErrorNum = CGGetActiveDisplayList(kMaxDisplays, displays, &numDisplays);
+	if (displayErrorNum) {
+		mexPrintf("(mglSetMousePosition) Cannot get display list (%d)\n", displayErrorNum);
+		return;
+	}
+
+	// Make sure the target screen is in bounds.
+	if (targetScreen < 0 || targetScreen >= numDisplays) {
+		mexPrintf("(mglSetMousePosition) targetScreen is out of bounds (%d)\n", targetScreen+1);
+		return;
+	}
+
+	// Get the pixel height of the target screen.
+	pxHeight = CGDisplayPixelsHigh(displays[targetScreen]);
 
 	cursorPos.x = (CGFloat)xPos;
-	cursorPos.y = (CGFloat)yPos;
-	
-	errorCode = CGWarpMouseCursorPosition(cursorPos);
-	
+
+	// We flip the y-coordinate to make it consistent with the Cocoa coordinate system
+	// where (0,0) is the lower left corner of the main screen.
+	cursorPos.y = (CGFloat)pxHeight - (CGFloat)yPos;
+
+	errorCode = CGDisplayMoveCursorToPoint(displays[targetScreen], cursorPos);
+	//errorCode = CGWarpMouseCursorPosition(cursorPos);
+
 	if (errorCode != kCGErrorSuccess) {
 		mexPrintf("(mglSetMousePosition) Setting mouse position failed with error code %d.\n", errorCode);
 		return;
 	}
 #endif
 }
+
