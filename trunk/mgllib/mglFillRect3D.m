885cfd0ccf5483e295823658bd04566e6aa0d879
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-09-19 20:26:52 +0000

    - Updated the help documentation.
    	CGB

--- trunk/mgllib/mglFillRect3D.m
+++ trunk/mgllib/mglFillRect3D.m
@@ -1,19 +1,19 @@
 % mglFillRect3D - draw filled rectangle(s) on the screen
 %
-%        $Id: mglFillRect.m 18 2006-09-13 15:41:18Z justin $
-%      usage: [  ] = mglFillRect(x, y, size, [rgb], [rotation], [antialias])
+%        $Id: mglFillRect3D.m 18 2006-09-13 15:41:18Z justin $
+%      usage: mglFillRect3D(x, y, z, size, [rgb], [rotation], [antialias])
 %         by: Christopher Broussard
 %       date: 05/10/2011
 %  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-%     inputs: x,y - vectors of x and y coordinates of center
+%     inputs: x,y,z - vectors of x, y, and z coordinates of center
 %             size - [width height] of oval
 %             rgb - [r g b] triplet for color
 %             rotation - [angle axisOfRotation] Rotation parameters
 %             antialias - Toggles antialiasing
 %
-%    purpose: Draw filled rectangles(s) centered at x,y with size [xsize
+%    purpose: Draw filled rectangles(s) centered at x,y,z with size [xsize
 %    ysize] and color [rgb]. The function is vectorized, so if you
-%    provide many x/y coordinates (identical) ovals will be plotted
+%    provide many x/y/z coordinates (identical) ovals will be plotted
 %    at all those locations.  Rotation is a 4 element vector that specifies
 %    the amount of rotation in degrees and the vector about which it rotates.
 %    Rotation is performed about the center of each rectangle.
@@ -24,7 +24,8 @@
 % mglVisualAngleCoordinates(57,[16 12]);
 % x = [-1 -4 -3 0 3 4 1];
 % y = [-1 -4 -3 0 3 4 1];
+% z = [-1 -4 -3 0 3 4 1];
 % sz = [1 1]; 
 % rotData = [45 0 1 0];  % 45 degrees about the y-axis.
-% mglFillRect(x, y, sz, [1 1 0], rotData);
+% mglFillRect3D(x, y, z, sz, [1 1 0], rotData);
 % mglFlush();
