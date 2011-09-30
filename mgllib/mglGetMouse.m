40a1466e881dc50107a66e5d42c6fd40140e4e7e
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-09-30 16:28:04 +0000

    - Updated mglGetMouse to have the ability to return mouse coordinates relative to a display if specified. - Didn't recompile anything since I don't have Lion.
    	CGB

--- mgllib/mglGetMouse.m
+++ mgllib/mglGetMouse.m
@@ -1,12 +1,20 @@
 % mglGetMouse.m
 %
 %        $Id$
-%      usage: mglGetMouse()
+%      usage: mglGetMouse
 %         by: justin gardner
 %       date: 09/12/06
 %  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-%    purpose: returns the status of the mouse buttons (regardless
-%             of whether the focuse is on the mgl window
-%       e.g.:
-%mglGetMouse
+%    purpose: Return state of mouse buttons and its position.  Position is
+%             in global screen coordinates unless a target screen is
+%             specified.
+%      usage: mglGetMouse([targetScreen])
+%           
+%             % Get mouse info in global screen coordinates.
+%             mouseInfo = mglGetMouse;
+% 
+%             % Get mouse info in screen 2 coordinates.
+%             mouseInfo = mglGetMouse(2);
+
+
 
