5d5a287deafca787c7be8785b909d8d65750622c
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-10-24 20:52:01 +0000

    Framebuffer objects now support multisampling.
    	CGB

--- trunk/mgllib/mglBindFrameBufferObject.c
+++ trunk/mgllib/mglBindFrameBufferObject.c
@@ -7,7 +7,7 @@
    copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
      purpose: Binds a frame buffer object to the current OpenGL context and
 			  directs all subsequent OpenGL calls to the frame buffer object.
-       usage: mglBindFrameBufferObject(fboObject)
+       usage: mglBindFrameBufferObject(fbObject)
 
 $Id: mglBindFrameBufferObject.c 18 2010-09-07 15:41:18Z cgb $
 =========================================================================
@@ -25,18 +25,18 @@ $Id: mglBindFrameBufferObject.c 18 2010-09-07 15:41:18Z cgb $
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
   mxArray *field;
-  GLuint fboID;
-  int width, height;
+  GLuint fboID, mfboID;
+  int width, height, enableMultisampling;
   
   if (nrhs != 1) {
 	  usageError("mglBindFrameBufferObject");
-      return;
+    return;
   }
   
   // Make sure the input was a struct.
   if (!mxIsStruct(prhs[0])) {
 	  usageError("mglBindFrameBufferObject");
-      return;
+    return;
   }
   
   // Get the frame buffer object ID and its dimensions.
@@ -46,10 +46,23 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   width = (GLuint)mxGetScalar(field);
   field = mxGetField(prhs[0], 0, "height");
   height = (GLuint)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "multisampling");
+  enableMultisampling = (int)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "mid");
+  mfboID = (GLuint)mxGetScalar(field);
   
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
+  // If multisampling is enabled, we need to bind our multisampled
+  // framebuffer object.  We'll copy to the normal framebuffer object
+  // later in the mglUnbindFrameBufferObject function.
+  if (enableMultisampling) {
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, mfboID);
+  }
+  else {
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
+  }
 
   // Save the view port and set it to the size of the texture
   glPushAttrib(GL_VIEWPORT_BIT);
   glViewport(0, 0, width, height);
 }
+
--- trunk/mgllib/mglUnbindFrameBufferObject.c
+++ trunk/mgllib/mglUnbindFrameBufferObject.c
@@ -7,7 +7,7 @@
    copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
      purpose: Unbinds a frame buffer object from the current OpenGL context
 			  to allow rendering to the standard frame buffer.
-       usage: mglUnbindFrameBufferObject
+       usage: mglUnbindFrameBufferObject(fbObject)
 
 $Id: mglUnbindFrameBufferObject.c 18 2010-09-07 15:41:18Z cgb $
 =========================================================================
@@ -24,6 +24,45 @@ $Id: mglUnbindFrameBufferObject.c 18 2010-09-07 15:41:18Z cgb $
 //////////////
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
+  mxArray *field;
+  GLuint fboID, mfboID;
+  int width, height, enableMultisampling;
+
+  if (nrhs != 1) {
+	  usageError("mglUnbindFrameBufferObject");
+    return;
+  }
+  
+  // Make sure the input was a struct.
+  if (!mxIsStruct(prhs[0])) {
+	  usageError("mglUnbindFrameBufferObject");
+    return;
+  }
+  
+  // Get the frame buffer object ID and its dimensions.
+  field = mxGetField(prhs[0], 0, "id");
+  fboID = (GLuint)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "width");
+  width = (GLuint)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "height");
+  height = (GLuint)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "multisampling");
+  enableMultisampling = (int)mxGetScalar(field);
+  field = mxGetField(prhs[0], 0, "mid");
+  mfboID = (GLuint)mxGetScalar(field);
+
 	glPopAttrib();
+
+  // If multisampling is enabled, we need to copy the multisampled framebuffer
+  // object to the regular one.  Apparently, this is needed for the graphics card
+  // to downsample the scene properly.  I don't know all the details behind this.
+  if (enableMultisampling) {
+    glBindFramebufferEXT(GL_READ_FRAMEBUFFER_EXT, mfboID);
+    glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER_EXT, fboID);
+    glBlitFramebufferEXT(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+  }
+
+  // Unbind all framebuffer objects.
 	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
 }
+
