19108ca550291c7e17dd7deeb5db549b1202fa7f
chrg <chrg@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2011-10-24 18:27:29 +0000

    mglEyeLinkGetCurrentEyePos now also returns the timestamp associated with the eye position.
    	CGB

--- mgllib/mglEyelink/mglEyelinkGetCurrentEyePos.m
+++ mgllib/mglEyelink/mglEyelinkGetCurrentEyePos.m
@@ -1,20 +1,41 @@
-function [pos] = mglEyelinkGetCurrentEyePos(devicecord)
+function [pos, posTime] = mglEyelinkGetCurrentEyePos(devicecord)
+% mglEyelinkGetCurrentEyePos - Gets the current eye position and time.
+%
+% Syntax:
+% [pos, posTime] = mglEyelinkGetCurrentEyePos
+% [pos, posTime] = mglEyelinkGetCurrentEyePos(devicecord)
+%
+% Description:
+% Returns the current eye position and its time stamp.
+%
+% Input:
+% devicecord (logical) - If true, the eye position is converted into MGL
+%   device coordinates.  Defaults to true.
+%
+% Output:
+% pos (1x2) - (x,y) position of the eye in EyeLink tracker coordinates.  Is
+%   returned as [NaN,NaN] if no sample is available.
+% posTime (scalar) - The timestamp of the position from the EyeLink.
+%   Returned as NaN if no timestamp is available.
 
-    if nargin < 1
-        devicecord = 1;
-    end
-    
-    sample = mglPrivateEyelinkGetCurrentSample();
-    if ~isempty(sample)
-        if devicecord
-            pos(1) = (sample(1)-(mglGetParam('screenWidth')/2))*mglGetParam('xPixelsToDevice');
-            pos(2) = ((mglGetParam('screenHeight')/2)-sample(2))*mglGetParam('yPixelsToDevice');
-        else
-            pos(1) = sample(1);
-            pos(2) = sample(2);
-        end
-    else
-        pos = [NaN NaN];
-    end
-    
+if nargin < 1
+	devicecord = 1;
+end
+
+% Get the current eye sample.
+sample = mglPrivateEyelinkGetCurrentSample;
+
+if ~isempty(sample)
+	if devicecord
+		pos(1) = (sample(1)-(mglGetParam('screenWidth')/2))*mglGetParam('xPixelsToDevice');
+		pos(2) = ((mglGetParam('screenHeight')/2)-sample(2))*mglGetParam('yPixelsToDevice');
+	else
+		pos(1) = sample(1);
+		pos(2) = sample(2);
+	end
+	
+	posTime = sample(8);
+else
+	pos = [NaN, NaN];
+	posTime = NaN;
 end
