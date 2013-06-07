9ac547f4388ac54f1bffdbf4fa0d6700d0ae989c
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2013-06-07 02:32:36 +0000

    Removing reference to eyelink frameworks which are not needed (and would cause compilation to fail on a machine that does not have them installed

--- trunk/mgllib/mexopts.10.8.xcode.4.5.sh
+++ trunk/mgllib/mexopts.10.8.xcode.4.5.sh
@@ -224,9 +224,7 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
             SDKROOT='/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk'
             MACOSX_DEPLOYMENT_TARGET='10.8'
             ARCHS='x86_64'
-#            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -pthread -I$HOME/Library/Frameworks/eyelink_core.framework/Headers -I$HOME/Library/Frameworks/edfapi.framework/Headers -I/Library/Frameworks/eyelink_core.framework/Headers -I/Library/Frameworks/edfapi.framework/Headers"
-#            CFLAGS="$CFLAGS  -fexceptions"
-            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -pthread -I$HOME/Library/Frameworks/eyelink_core.framework/Headers -I$HOME/Library/Frameworks/edfapi.framework/Headers -I/Library/Frameworks/eyelink_core.framework/Headers -I/Library/Frameworks/edfapi.framework/Headers"
+            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -pthread"
             CFLAGS="$CFLAGS  -fexceptions"
             CLIBS="$MLIBS"
             COPTIMFLAGS='-O2 -DNDEBUG'
@@ -259,8 +257,7 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
             LD="$CC"
             LDEXTENSION='.mexmaci64'
             LDFLAGS="-Wl,-twolevel_namespace -undefined error -arch $ARCHS -Wl,-syslibroot,$SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-#           LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE  -framework agl -framework Carbon -framework Cocoa -framework CoreServices -framework openGL -framework eyelink_core -framework edfapi  -pthread -framework QTKit"
-            LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -F$HOME/Library/Frameworks -framework agl -framework Carbon -framework Cocoa -framework CoreServices -framework openGL -framework eyelink_core -framework edfapi -pthread -framework QTKit"
+            LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -F$HOME/Library/Frameworks -framework agl -framework Carbon -framework Cocoa -framework CoreServices -framework openGL -pthread -framework QTKit"
             LDOPTIMFLAGS='-O'
             LDDEBUGFLAGS='-g'
 #
