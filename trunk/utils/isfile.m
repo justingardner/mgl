904000c04ee7cad15af591d0fbe8115d18a4ef95
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2013-11-20 05:17:41 +0000

    Fix to report only files that match the path correctly (otherwise matlab looks all through its own path)

--- trunk/utils/isfile.m
+++ trunk/utils/isfile.m
@@ -13,12 +13,25 @@ if (nargin ~= 1)
   help isfile;
   return
 end
+if isempty(filename),isit = 0;,return,end
 
 % open file
 fid = fopen(filename,'r');
 
 % check to see if there was an error
 if (fid ~= -1)
+  % check to see if this is the correct path (cause matlab can open
+  % files anywhere in the path)
+  if (length(filename)>=1) && ~isequal(filename(1),filesep)
+    openname = fopen(fid);
+    if ~strcmp(fullfile(pwd,filename),openname)
+      %disp(sprintf('(isfile) Found file %s, but not in current path',openname));
+      isit = false;
+      fclose(fid);
+      return;
+    end
+  end
+  % close file and get permissions
   fclose(fid);
   [dummy permission] = fileattrib(filename);
   isit = true;
