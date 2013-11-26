75c0290bc722e02c4fa98fb10d5cee1cdd935607
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2013-11-26 00:27:33 +0000

    This should now work by saving the keycodeToChar in the param - also brought back the mexmaci for the old version in 32 bit since that one is not tested on my system which no longer has a 32 bit matlab

--- trunk/mgllib/mglKeycodeToChar.m
+++ trunk/mgllib/mglKeycodeToChar.m
@@ -27,13 +27,19 @@ function keyname=mglKeycodeToChar(keycode)
 % 
 % jl 20061020
 
+% check input arguments
+if nargin ~= 1
+  help mglKeycodeToChar;
+  keyname = [];
+  return
+end
 % check to see if there is a stored keycodeToChar array
 keycodeToChar = mglGetParam('keycodeToChar');
 if isempty(keycodeToChar)
   % get the mapping of keycodes to chars
   keycodeToChar = mglPrivateKeycodeToChar(1:128);
   % add a blank keycode for out of range
-  keycodeToChar(129) = '';
+  keycodeToChar{129} = [];
   % store in mgl param
   mglSetParam('keycodeToChar',keycodeToChar);
 end
@@ -42,7 +48,7 @@ end
 if any(keycode<1) || any(keycode>128)
   disp(sprintf('(mglKeycodeToChar) Keycode out of range'));
   % set to the special out of range code
-  keycode((keycode<1) || (keycode>128)) = 129;
+  keycode((keycode<1) | (keycode>128)) = 129;
 end
 
 % now lookup mapping and retrun
