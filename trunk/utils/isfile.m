690e3aca08852a1a242abf764914ba1154ec1a89
justin <justin@f986d8f3-ee5e-0410-9c00-b90a3c77adb6>
2014-01-06 04:40:15 +0000

    isfile now can handle tilde correctly (i.e. search in home directory). mglEditScreenParams gets minor fix to close screen after testing.

--- trunk/utils/isfile.m
+++ trunk/utils/isfile.m
@@ -15,6 +15,9 @@ if (nargin ~= 1)
 end
 if isempty(filename),isit = 0;,return,end
 
+% replace the tilde with a full path
+filename = mglReplaceTilde(filename);
+
 % open file
 fid = fopen(filename,'r');
 
@@ -37,5 +40,35 @@ if (fid ~= -1)
   isit = true;
 else
   isit = false;
+end
+
+
+% mglReplaceTilde.m
+%
+%        $Id:$ 
+%      usage: filename =  mglReplaceTilde(filename)
+%         by: justin gardner
+%       date: 12/07/11
+%    purpose: If filename starts with a tilde, will replace with
+%             fully qualified path (e.g. ~/uhm will become /Users/justin/uhm)
+%
+function filename = mglReplaceTilde(filename)
+
+% check arguments
+if ~any(nargin == [1])
+  help mglReplaceTilde
+  return
+end
+
+if (length(filename) >= 1) && (filename(1) == '~')
+  % get tilde dir
+  if ispc
+    tildeDir = [getenv('HOMEDRIVE') getenv('HOMEPATH')];
+  else
+    tildeDir = getenv('HOME');
+  end
+
+  % apend tildeDir
+  filename = fullfile(tildeDir,filename(2:end));
 end
 
