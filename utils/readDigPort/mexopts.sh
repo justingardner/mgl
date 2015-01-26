e81090c3cda6662bf6d5390ecf3c4ba539739474
Justin Gardner <jlg@stanford.edu>
2015-01-26 14:50:43 -0800

    Something bad about the old mexopts.sh file this one works

--- utils/readDigPort/mexopts.sh
+++ utils/readDigPort/mexopts.sh
@@ -56,7 +56,7 @@
 #           gcc -v
 #           gcc version 3.2.3
             CC='g++'
-            CFLAGS='-fPIC -ansi -D_GNU_SOURCE -pthread -fexceptions -m32'
+            CFLAGS='-fPIC -ansi -D_GNU_SOURCE -fexceptions -m32'
             CLIBS="$RPATH $MLIBS -lm -lstdc++ -L/usr/X11R6/lib -lXext -lX11 -lXi -lXmu -lGL -lGLU"
             COPTIMFLAGS='-O -DNDEBUG'
             CDEBUGFLAGS='-g'
@@ -64,7 +64,7 @@
 #           g++ -v
 #           gcc version 3.2.3
             CXX='g++'
-            CXXFLAGS='-fPIC -ansi -D_GNU_SOURCE -pthread -DGLX_GLXEXT_LEGACY'
+            CXXFLAGS='-fPIC -ansi -D_GNU_SOURCE -DGLX_GLXEXT_LEGACY'
             CXXLIBS="$RPATH $MLIBS -lm -L/usr/X11R6/lib -lXext -lX11 -lXi -lXmu -lGL -lGLU"
             CXXOPTIMFLAGS='-O -DNDEBUG'
             CXXDEBUGFLAGS='-g'
@@ -98,7 +98,7 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
 #           gcc -v
 #           gcc version 3.2.3
             CC='gcc'
-            CFLAGS='-fPIC -fno-omit-frame-pointer -ansi -D_GNU_SOURCE -pthread -fexceptions'
+            CFLAGS='-fPIC -fno-omit-frame-pointer -ansi -D_GNU_SOURCE -fexceptions'
             CLIBS="$RPATH $MLIBS -lm -lstdc++"
             COPTIMFLAGS='-O -DNDEBUG'
             CDEBUGFLAGS='-g'
@@ -106,7 +106,7 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
 #           g++ -v
 #           gcc version 3.2.3
             CXX='g++'
-            CXXFLAGS='-fPIC -fno-omit-frame-pointer -ansi -D_GNU_SOURCE -pthread '
+            CXXFLAGS='-fPIC -fno-omit-frame-pointer -ansi -D_GNU_SOURCE '
             CXXLIBS="$RPATH $MLIBS -lm"
             CXXOPTIMFLAGS='-O -DNDEBUG'
             CXXDEBUGFLAGS='-g'
@@ -183,7 +183,7 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
 
 #            CC='g++-3.3'
             CC='gcc-4.0'
-            CFLAGS='-x objective-c -fno-common -no-cpp-precomp -fexceptions -arch i386 -pthread'
+            CFLAGS='-x objective-c -fno-common -no-cpp-precomp -fexceptions -arch i386'
             CLIBS="$MLIBS -lstdc++"
             COPTIMFLAGS='-O3 -DNDEBUG'
             CDEBUGFLAGS='-g'
@@ -220,11 +220,9 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
             # CkeyLanguage: C
             # CkeyVersion:
             CC='gcc'
-#            CC='g++-4.0'
+            CC='g++'
             ARCHS='x86_64'
-#            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS"
-#            CFLAGS="$CFLAGS  -fexceptions"
-            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS"
+            CFLAGS="-x objective-c -fno-common -no-cpp-precomp -arch $ARCHS -Wno-deprecated-declarations -Wno-deprecated -Wno-implicit-function-declaration"
             CFLAGS="$CFLAGS  -fexceptions"
             CLIBS="$MLIBS"
             COPTIMFLAGS='-O2 -DNDEBUG'
@@ -256,9 +254,8 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh glnxi64 
 #
             LD="$CC"
             LDEXTENSION='.mexmaci64'
-            LDFLAGS="-Wl,-twolevel_namespace -undefined error -arch $ARCHS -Wl,-syslibroot"
-#           LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE  -framework agl -framework Carbon -framework Cocoa -framework CoreServices -framework openGL"
-            LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -framework Carbon -framework Cocoa -framework CoreServices -framework openGL"
+            LDFLAGS="-Wl,-twolevel_namespace -undefined error -arch $ARCHS -Wl"
+            LDFLAGS="$LDFLAGS -bundle -Wl,-exported_symbols_list,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -framework agl -framework Carbon -framework Cocoa -framework CoreServices -framework openGL -framework QTKit"
             LDOPTIMFLAGS='-O'
             LDDEBUGFLAGS='-g'
 #
