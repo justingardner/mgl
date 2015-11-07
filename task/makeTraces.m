b1801775f1e5004b70aa3af9e2ebca538dece11d
Justin Gardner <jlg@stanford.edu>
2015-11-07 12:59:06 -0800

    Fix for when ticknum was incorrectly set

--- task/makeTraces.m
+++ task/makeTraces.m
@@ -39,6 +39,22 @@ if (isfield(myscreen,'events'))
   for i = 1:myscreen.events.n
     % get the tick num for this event
     ticknum = myscreen.events.ticknum(i);
+
+    % ticknum shouldn't be nan - but when spoofing volumes, some files
+    % have this set to nan - if they were added after the end of the original
+    % stimfile - so, fixing that here (also fixing in dofmricni which caused
+    % the problem)
+    if isnan(ticknum)
+      % get the last event with a valid ticknum
+      lastGoodEvent = find(~isnan(myscreen.events.ticknum(1:myscreen.events.n)));
+      lastGoodEvent = lastGoodEvent(end);
+      % get last ticknum
+      lastGoodTicknum = myscreen.events.ticknum(lastGoodEvent);
+      % compare its time to this event time and multiply that by ticksPerSecond
+      % and add to the ticknum from that last good event to get the ticknum
+      % that should have been put in this event
+      ticknum = lastGoodTicknum + round((myscreen.events.time(i) - myscreen.events.time(lastGoodEvent))*myscreen.framesPerSecond);
+    end
     % put the data into the trace
     % if it is a force, then only set the current one
     if myscreen.events.force(i)
