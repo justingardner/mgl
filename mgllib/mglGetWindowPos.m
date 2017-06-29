6c1be0656db996617afb3d63c5c84a08266284b2
JustinGardner <jshopsafe@me.com>
2017-06-29 09:50:50 +0900

    Function to get windowed context size

---
+++ mgllib/mglGetWindowPos.m
@@ -0,0 +1,15 @@
+% mglGetWindowPos.m
+%
+%      usage: windowPos = mglGetWindowPos
+%         by: Justin Gardner
+%       date: 07/29/17
+%  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
+%    purpose: Returns the position of a windowed context opened with mglOpen(0)
+%             returned position will be in pixels [x y width height]
+%
+%      usage: windowPos = mglGetWindowPos
+%             
+%
+% mglOpen(0);
+% windowPos = mglGetWindowPos
+% 
