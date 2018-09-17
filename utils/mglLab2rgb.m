aaca7414b66e1366f210742032df220c9784f53d
gru <gru-lab>
2018-09-17 11:39:24 -0700

    Adding a utility function mglLab2rgb which uses the calibration color conversion matrices

---
+++ utils/mglLab2rgb.m
@@ -0,0 +1,32 @@
+function rgb = mglLab2rgb(Lab,calib)
+%% MGLLAB2RGB
+%
+%   Input:
+%    Lab - a nx3 matrix of L*a*b* coordinates (CIELAB)
+%    calib - a myscreen.calib structure with a spectrum calibrated xyz2rgb
+%            matrix
+%
+%   Output:
+%    rgb - a nx3 matrix of RGB coordinates
+%
+
+if isempty(calib) || ~isfield(calib,'colors') || ~isfield(calib.colors,'XYZ2RGB')
+    oneTimeWarning('noxyz2rgb','Calibration doesn''t include a calculated color conversion matrix for XYZ -> RGB. Try running moncalib with spectrum=1 and color=1. Returning matlab lab2rgb values which are *NOT CALIBRATED*');
+    rgb = lab2rgb(Lab);
+    return
+end
+
+if size(Lab,2)~=3
+    warning('Lab input size is not nx3');
+    return
+end
+
+rgb = zeros(size(Lab));
+
+for i = 1:size(Lab,1)
+    xyz = lab2xyz(Lab(i,:));
+    if size(xyz,1)~=3
+        xyz = xyz';
+    end
+    rgb(i,:) = calib.colors.XYZ2RGB*xyz;
+end
