bc7523993c9aa1ddf711944b0b73757f7e77a6ab
JustinGardner <jshopsafe@me.com>
2020-01-07 14:55:52 -0800

    Adding some profiling code

---
+++ mglMetal/mglSecs.h
@@ -0,0 +1,23 @@
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//
+//  mglGetSecs.h
+//  mglMetal
+//
+//  Created by justin gardner on 1/5/20.
+//  Copyright © 2020 GRU. All rights reserved.
+//
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+// class which returns a double that reprsesent
+// the same value that mglGetSecs returns in matlab
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+@interface mglGetSecs : NSObject
+-(double) get;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ mglMetal/mglSecs.m
@@ -0,0 +1,34 @@
+//
+//  mglGetSecs.m
+//  mglMetal
+//
+//  Created by justin gardner on 1/5/20.
+//  Copyright © 2020 GRU. All rights reserved.
+//
+
+#import "mglGetSecs.h"
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+
+@implementation mglGetSecs
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+// mglGetSecs: get
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+-(double) get
+{
+      static const double kOneBillion = 1000 * 1000 * 1000;
+      static mach_timebase_info_data_t sTimebaseInfo;
+
+      if (sTimebaseInfo.denom == 0) {
+        (void) mach_timebase_info(&sTimebaseInfo);
+      }
+      // This seems to work on Mac OS 10.9 with a Mac PRO. But note that sTimebaseInfo is hardware implementation
+      // dependent. The mach_absolute_time is ticks since the machine started and to convert it to ms you
+      // multiply by the fraction in sTimebaseInfo - worried that this could possibly overflow the
+      // 64 bit int values depending on what is actually returned. Maybe that is not a problem
+      double currtime = ((double)((mach_absolute_time()*(uint64_t)(sTimebaseInfo.numer)/(uint64_t)(sTimebaseInfo.denom)))/kOneBillion);
+    printf("(mglGetSecs) Current time is: %f\n",currtime);
+    return(currtime);
+}
+@end
