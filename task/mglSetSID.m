e6ca9e418e1bec1087c87c1ff02d565c79fa5c2f
Justin Gardner <jlg@stanford.edu>
2020-05-19 17:16:31 -0700

    Dealing with first database

--- task/mglSetSID.m
+++ task/mglSetSID.m
@@ -786,7 +786,7 @@ while ~validated
 end
 
 % save the database
-if ~isempty(params) && ~isempy(sidDatabase.sid)
+if ~isempty(params) && ~isempty(sidDatabase.sid)
   % check for repeat by first/last name
   firstIdx = cellfun(@(x) strcmp(x,params.firstName),sidDatabase.firstName);
   lastIdx = cellfun(@(x) strcmp(x,params.lastName),sidDatabase.lastName);
