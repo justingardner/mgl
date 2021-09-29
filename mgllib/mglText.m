88eaba8f4f7738c6184ed310783980d4aa39def2
Justin Gardner <jlg@stanford.edu>
2021-09-28 22:17:39 -0700

    mglText now gets the image text from thep rivate function and then calls mglMetalCreateTexture

--- mgllib/mglText.m
+++ mgllib/mglText.m
@@ -1,13 +1,13 @@
 % mglText.m
 %
 %        $Id$
-%      usage: mglText('string')
+%      usage: tex = mglText('string')
 %         by: justin gardner
-%       date: 05/10/06
-%  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
-%    purpose: Returns image of string.
+%       date: 09/28/2021 Based on version from 05/10/06
+%  copyright: (c) 2021 Justin Gardner (GPL see mgl/COPYING)
+%    purpose: Returns a texture for a string
 %
-%       e.g.: 
+%       e.g.:
 %
 %mglOpen
 %mglVisualAngleCoordinates(57,[16 12]);
@@ -15,4 +15,19 @@
 %thisText = mglText('Hello')
 %mglBltTexture(thisText,[0 0],'left','top');
 %mglFlush;
+function tex = mglText(str)
 
+if (nargin ~= 1) || ~isstr(str)
+  help mglText;
+  return
+end
+
+% get the image of the text
+im = mglPrivateText(str);
+
+% need to reshape
+im.textImage = reshape(im.textImage,im.imageHeight,im.imageWidth,4);
+im.textImage = permute(im.textImage,[2 1 3]);
+
+% create the texture
+tex = mglMetalCreateTexture(im.textImage);
