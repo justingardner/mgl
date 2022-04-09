2b2748e1976f2ab540ff09e4719077e52260feb1
Justin Gardner <jlg@stanford.edu>
2022-04-09 13:55:33 -0700

    this would return true even if mglMetal was not running - seems to happen because of weird behavior of the matlab system command which returns things that are in the text buffer - i.e. the command was trying to do a ps to check if mglMetal was running and checked if the return was empty, but instead it was full of the next few commands the user was going to run. Fixed it to check for the string mglMetal.app and that seems to have fixed it.

--- mgllib/mglMetalIsRunning.m
+++ mgllib/mglMetalIsRunning.m
@@ -19,7 +19,11 @@ psid = [];
 % check if mglMetal is running
 [status, isMetalRunning] = system('ps aux | grep -i mglMetal | grep -v grep');
 
-if ~isempty(isMetalRunning)
+% check whether the string mglMetal.app is in the return - this
+% is necessary because the system command also returns anything
+% that is in the text buffer - for example, if you copy and
+% paste a set of commands, for some reason that is in the return of system
+if ~isempty(strfind(isMetalRunning,'mglMetal.app'))
   tf = true;
   [~,isMetalRunning] = strtok(isMetalRunning);
   [psid] = strtok(isMetalRunning);
