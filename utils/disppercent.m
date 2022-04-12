710b2719cb9a862e328e31d6b227da8da322b37d
Justin Gardner <jlg@stanford.edu>
2022-04-12 13:39:49 -0700

    Fixing disppercent to work with tput system command

--- utils/disppercent.m
+++ utils/disppercent.m
@@ -27,7 +27,7 @@
 %             Also, if you have an inner loop within an outer loop, you
 %             can call like the following:
 %             n1 = 15;n2 = 10;
-%             disppercent(-1/n1); % init with how much the outer loop increments
+%             disppercent(-1/n1,'Testing disppercent'); % init with how much the outer loop increments
 %             for i = 1:n1
 %               for j = 1:n2
 %                  pause(0.1);
@@ -37,6 +37,10 @@
 %             end
 %             disppercent(inf);
 %
+%              To test call:
+%
+%             disppercent test
+% 
 %       e.g.:
 %
 %disppercent(-inf,'Doing stuff');for i =  1:30;pause(0.1);disppercent(i/30);end;elapsedTime = disppercent(inf);
@@ -52,6 +56,20 @@ end
 % global for disppercent
 global gDisppercent;
 
+% check for tput (system command that allows for updating text on
+% terminals)
+if ~isfield(gDisppercent,'hastput')
+  [gDisppercent.hastput,~] = system('which tput');
+  gDisppercent.hastput = ~gDisppercent.hastput;
+end
+
+% test disppercent
+if (isequal(percentdone,'test'))
+  testDisppercent;
+  return
+end
+
+
 % if this is an init then remember time
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if (percentdone < 0)
@@ -61,14 +79,18 @@ if (percentdone < 0)
   if ~gVerbose,return,end
   % set starting time
   gDisppercent.t0 = clock;
+  % clear previous message and set to special startup message
+  gDisppercent.mesg = '__startup__';
   % default to no message
   if (nargin < 2)
-    fprintf(1,sprintf('00%% (00:00:00)'));
-    gDisppercent.mesg = '';
+    % display message (saving cursor loc at end)
+    dispNoNewline(true,'');
   else
-    fprintf(1,sprintf('%s 00%% (00:00:00)',mesg));
-    gDisppercent.mesg = mesg;
+    % display message (saving cursor loc at end)
+    dispNoNewline(true,sprintf('%s: ',mesg));
   end    
+  % display time 
+  dispNoNewline(false,sprintf('00%%%% (00:00:00)'));
   if isinf(percentdone)
     gDisppercent.increment = 0;
   else
@@ -81,7 +103,7 @@ elseif (percentdone == inf)
   if ~gDisppercent.verbose,return,end
   % reprint message if necessary
   if (nargin == 2) && isstr(mesg)
-    reprintMessage(mesg);
+    dispNoNewline(true,mesg);
   end
   % get elapsed time
   elapsedTime = etime(clock,gDisppercent.t0);
@@ -104,7 +126,11 @@ elseif (percentdone == inf)
     timestr = sprintf('%i secs %i ms',numsecs,numms);
   end
   % display time string
-  fprintf(1,sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\btook %s\n',timestr));
+  if gDisppercent.hastput
+    dispNoNewline(false,sprintf('took %s\n',timestr));
+  else
+    fprintf(sprintf('took %s\n',timestr));
+  end
   retval = elapsedTime;
 % otherwise show update
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -125,13 +151,13 @@ else
   newmesg = '';
   if nargin == 2
     if isstr(mesg)
-      newmesg = reprintMessage(mesg);
-    % otherwise if the second argument is a number,
-    % it means we have a secondary value for percent done
-    % i.e. the first number is the large increments and
-    % the second number is what percentage of the large
-    % increments has been done (useful for when you are
-    % doing loops within loops).
+      newmesg = sprintf('%s: ',mesg);
+      % otherwise if the second argument is a number,
+      % it means we have a secondary value for percent done
+      % i.e. the first number is the large increments and
+      % the second number is what percentage of the large
+      % increments has been done (useful for when you are
+      % doing loops within loops).
     elseif isscalar(mesg)
       % make percent done into value computed by summing
       % percentdone with the increment passed in mesg.
@@ -149,10 +175,11 @@ else
   % display percent done and estimated time to end
   if ~isempty(newmesg)
     % always display if there is a new message
-    fprintf(1,sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b%s%02i%% (%s)',newmesg,floor(100*percentdone),disptime(etime(clock,gDisppercent.t0)*(1/percentdone - 1))));
+    dispNoNewline(true,newmesg);
+    dispNoNewline(false,sprintf('%02i%%%% (%s)',floor(100*percentdone),disptime(etime(clock,gDisppercent.t0)*(1/percentdone - 1))));
   % display only if we have update by a percent or more
   elseif (gDisppercent.percentdone ~= floor(100*percentdone))
-    fprintf(1,sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b%02i%% (%s)',floor(100*percentdone),disptime(etime(clock,gDisppercent.t0)*(1/percentdone - 1))));
+    dispNoNewline(false,sprintf('%02i%%%% (%s)',floor(100*percentdone),disptime(etime(clock,gDisppercent.t0)*(1/percentdone - 1))));
   end
 end
 % remember current percent done
@@ -170,23 +197,74 @@ seconds = floor(t-hours*60*60-minutes*60);
 
 retval = sprintf('%02i:%02i:%02i',hours,minutes,seconds);
 
-%%%%%%%%%%%%%%%%%%%%%%%%
-%%   reprintMessage   %%
-%%%%%%%%%%%%%%%%%%%%%%%%
-function newmesg = reprintMessage(mesg);
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% Display w/out newline
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function dispNoNewline(saveCursorLoc,str)
 
+% get global
 global gDisppercent;
 
-newmesg = '';
-
-if ~strcmp(mesg,gDisppercent.mesg)
-  % first clear old message
-  fprintf(1,sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b%s%s              ',repmat(sprintf('\b'),1,length(gDisppercent.mesg)+1),repmat(sprintf(' '),1,length(gDisppercent.mesg)+1)));
-  % print <or return> new message
-  if nargout == 1
-    newmesg = sprintf('%s%s ',repmat(sprintf('\b'),1,length(gDisppercent.mesg)+1),mesg);
+% For systems without tput, just display the initial message
+% and increment with a .
+if ~gDisppercent.hastput
+  if saveCursorLoc & isequal(gDisppercent.mesg,'__startup__')
+    fprintf(str);
+    gDisppercent.mesg = str;
   else
-    fprintf(1,sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b%s%s               ',repmat(sprintf('\b'),1,length(gDisppercent.mesg)+1),mesg));
+    fprintf('.');
   end
-  gDisppercent.mesg = mesg;
+  return
 end
+
+
+if ~saveCursorLoc
+  if gDisppercent.hastput
+    % restore the cursor loc to overwrite what was written
+    system('tput rc');
+  end
+else
+  % move to the beginning of old message   
+  if ~isequal(gDisppercent.mesg,'__startup__')
+    if gDisppercent.hastput
+      system('tput rc');
+      system(sprintf('tput cub %i',length(gDisppercent.mesg)));
+    end
+  end
+end
+
+% print the string w/out the backspace
+fprintf(str);
+
+if saveCursorLoc
+  if gDisppercent.hastput
+    % save the cursor loc after printing
+    system('tput sc');
+  end
+  % save the message
+  gDisppercent.mesg = str;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% Display w/out newline
+%%%%%%%%%%%%%%%%%%%%%%%%%
+function testDisppercent
+      
+disppercent(-inf,'Testing disppercent');
+for i = 1:100
+  pause(0.001);
+  disppercent(i/100);
+end
+disppercent(inf);
+
+n1 = 15;n2 = 10;
+disppercent(-1/n1,"Testing dual loop"); % init with how much the outer loop increments
+for i = 1:n1
+  for j = 1:n2
+    pause(0.001);
+    disppercent((i-1)/n1,j/n2);
+  end
+  disppercent(i/n1,sprintf('Made it through %i/%i iterations of outer loop',i,n1));
+end
+disppercent(inf);
+
