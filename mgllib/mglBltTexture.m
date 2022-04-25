0bd7a059f7a75d92e0fb2916f09c8d97554a0fa4
Ben Heasly <benjamin.heasly@gmail.com>
2022-04-25 14:43:27 -0400

    mglBltTexture support arrays of textures and params, with m-code loop for now.

--- mgllib/mglBltTexture.m
+++ mgllib/mglBltTexture.m
@@ -7,9 +7,9 @@
 %  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
 %    purpose: Draw a texture to the screen in desired position.
 %
-%             texture: A texture structure created by mglCreateTexture or
-%                      mglText.
-%             position: Either a 2-vector [xpos ypos] or 
+%             tex: A texture structure created by mglCreateTexture or
+%                  mglText.
+%             position: Either a 2-vector [xpos ypos] or
 %                       4-vector [xpos ypos width height]
 %                       For 1d textures, you can also specify a 3-vector
 %                       [xpos ypos height]
@@ -18,11 +18,15 @@
 %             vAlignment = {-1 = top,0 = center,1 = bottom}
 %                          defaults to center
 %             rotation: rotation in degrees, defaults to 0
+%             width: alternative way to specify the displayed width, this
+%                    takes precidence over position(3), if given.
+%             height: alternative way to specify the displayed height, this
+%                    takes precidence over position(4), if given.
 %
 %             To display several textures at once, texture
 %             can be an array of n textures, position is nx2, or nx4
-%             and hAlignment, vAlignment and rotation are either
-%             a single value or an array of n:
+%             and hAlignment, vAlignment, rotation, width, and height are
+%             either a single value or an array of n:
 %       e.g.: multiple textures
 %
 % mglOpen
@@ -48,5 +52,95 @@
 % imageTex = mglCreateTexture(image1d);
 % mglBltTexture(imageTex,[0 0 5]);
 % mglFlush;
-function [ackTime, processedTime] = mglBltTexture(varargin)
-[ackTime, processedTime] = mglMetalBltTexture(varargin{:});
+function [ackTime, processedTime] = mglBltTexture(tex, position, hAlignment, vAlignment, rotation, phase, width, height)
+
+% if nargin < 2, position = [0 0]; end
+% if nargin < 3, hAlignment = 0; end
+% if nargin < 4, vAlignment = 0; end
+% if nargin < 5, rotation = 0; end
+% if nargin < 6, phase = 0; end
+% if nargin < 7
+%   if length(position)<3
+%     % get width in device coordinates
+%     width = tex.imageWidth*mglGetParam('xPixelsToDevice');
+%   else
+%     width = position(3);
+%   end
+% end
+% if nargin < 8
+%   if length(position)<4
+%     % get height in device coordinates
+%     height = tex.imageHeight*mglGetParam('yPixelsToDevice');
+%   else
+%     height = position(4);
+%   end
+% end
+
+% Expand arguments and/or defaults to match the number of textures.
+textureCount = numel(tex);
+if nargin < 2
+    position = zeros(textureCount, 2);
+elseif size(position, 1) == 1
+    position = repmat(position, textureCount, 1);
+end
+
+if nargin < 3
+    hAlignment = zeros(textureCount, 1);
+elseif numel(hAlignment) == 1
+    hAlignment = repmat(hAlignment, textureCount, 1);
+end
+
+if nargin < 4
+    vAlignment = zeros(textureCount, 1);
+elseif numel(vAlignment) == 1
+    vAlignment = repmat(vAlignment, textureCount, 1);
+end
+
+if nargin < 5
+    rotation = zeros(textureCount, 1);
+elseif numel(rotation) == 1
+    rotation = repmat(rotation, textureCount, 1);
+end
+
+if nargin < 6
+    phase = zeros(textureCount, 1);
+elseif numel(phase) == 1
+    phase = repmat(phase, textureCount, 1);
+end
+
+if nargin < 7
+    if size(position, 2) < 3
+        % default: get width in device coordinates
+        width = [tex.imageWidth] * mglGetParam('xPixelsToDevice');
+    else
+        % alternative way to specify, as part of position vector
+        width = position(:, 3);
+    end
+elseif numel(width) == 1
+    width = repmat(width, textureCount, 1);
+end
+
+if nargin < 8
+    if size(position, 2) < 4
+        % default: get height in device coordinates
+        height = [tex.imageHeight] * mglGetParam('yPixelsToDevice');
+    else
+        height = position(:, 4);
+    end
+elseif numel(height) == 1
+    height = repmat(height, textureCount, 1);
+end
+
+ackTime = zeros([1, textureCount]);
+processedTime = zeros([1, textureCount]);
+for ii = 1:textureCount
+    [ackTime(ii), processedTime(ii)] = mglMetalBltTexture( ...
+        tex(ii), ...
+        position(ii,:), ...
+        hAlignment(ii), ...
+        vAlignment(ii), ...
+        rotation(ii), ...
+        phase(ii), ...
+        width(ii), ...
+        height(ii));
+end
---
+++ mgllib/mglTestMetal/mglRenderingTests/mglTestTextureMultiple.m
@@ -0,0 +1,68 @@
+% mglTestTextureMultiple: an automated and/or interactive test for rendering.
+%
+%      usage: mglTestTextureMultiple(isInteractive)
+%         by: Benjamin Heasly
+%       date: 03/10/2022
+%  copyright: (c) 2006 Justin Gardner, Jonas Larsson (GPL see mgl/COPYING)
+%    purpose: Test rendering a smoothly varying texture.
+%      usage:
+%             % You can run it by hand with no args.
+%             mglTestTextureMultiple();
+%
+%             % Or mglRunRenderingTests can run it, in non-interactive mode.
+%             mglTestTextureMultiple(false);
+%
+function mglTestTextureMultiple(isInteractive)
+
+if nargin < 1
+    isInteractive = true;
+end
+
+if (isInteractive)
+    mglOpen();
+    cleanup = onCleanup(@() mglClose());
+end
+
+%% How to:
+
+% Create several textures, each showing a number.
+textureCount = 10;
+textureCell = cell(textureCount, 1);
+mglTextSet('Helvetica', 32, [1 1 1 1], 1, 0, 0, 0, 0, 0, 0);
+for ii = 1:textureCount
+    textureCell{ii} = mglText(num2str(ii));
+end
+textures = [textureCell{:}];
+
+% Choose positions along a diagonal.
+x = linspace(-0.9, 0.9, textureCount);
+y = linspace(-0.9, 0.9, textureCount);
+position = [x' y'];
+
+% Choose arbitrary alignments from the allowed values -1, 0, or 1.
+hAlignment = mod(1:textureCount, 3) - 1;
+vAlignment = circshift(hAlignment, 1);
+
+% Choose arbitrary rotations in degrees.
+rotation = linspace(0, 360, textureCount);
+
+% Currently phase doesn't make sense with text, it slides rgb under alpha.
+phase = 0;
+
+% Wobble the sizes.
+width = linspace(0.5, 1.5, textureCount) .* [textures.imageWidth] .* mglGetParam('xPixelsToDevice');
+height = linspace(1.5, 0.5, textureCount) .* [textures.imageHeight] .* mglGetParam('yPixelsToDevice');
+
+% Blt all the textures and per-texture parameters in one call.
+disp('There should be several textures, each a different digit, each a different position, rotation, etc.');
+mglBltTexture(textures, position, hAlignment, vAlignment, rotation, phase, width, height);
+
+mglFlush();
+
+for ii = 1:textureCount
+    mglDeleteTexture(textures(ii));
+end
+
+if (isInteractive)
+    mglPause();
+end
