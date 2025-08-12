46cb115eb8552d359a21238b35cc6105f130584c
tyler bonnen <joshryu0425@gmail.com>
2025-08-12 14:45:30 -0700

    Caching hostname, so this operation does not take too long - this was the problem with mglValidateScreenParams

--- task/mglGetHostName.m
+++ task/mglGetHostName.m
@@ -14,43 +14,56 @@ if ~any(nargin == [0])
   return
 end
 
-[retval hostname] = system('hostname');
-% sometimes there is more than one line (errors from csh startup)
-% so need to strip those off
-hostname = strread(hostname,'%s','delimiter','\n');
-hostname = hostname{end};
-if (retval == 0)
-  % get it again
-  [retval hostname2] = system('hostname');
-  hostname2 = strread(hostname2,'%s','delimiter','\n');
-  hostname2 = hostname2{end};
+% check global to see if we have a cached version
+global globalHostname
+
+if isempty(globalHostname)
+  [retval, hostname] = system('hostname');
+
+  % sometimes there is more than one line (errors from csh startup)
+  % so need to strip those off
+  hostname = strread(hostname, '%s', 'delimiter', '\n');
+  hostname = hostname{end};
+
   if (retval == 0)
-    % find the matching last characers
-    % this is necessary, because matlab's system command
-    % picks up stray key strokes being written into
-    % the terminal but puts those at the beginning of
-    % what is returned by stysem. so we run the
-    % command twice and find the matching end part of
-    % the string to get the hostrname
-    minlen = min(length(hostname),length(hostname2));
-    for k = 0:minlen
-      if (k < minlen)
-	if hostname(length(hostname)-k) ~= hostname2(length(hostname2)-k)
-	  break
-	end
+    % get it again
+    [retval, hostname2] = system('hostname');
+    hostname2 = strread(hostname2, '%s', 'delimiter', '\n');
+    hostname2 = hostname2{end};
+
+    if (retval == 0)
+      % find the matching last characters
+      % this is necessary, because matlab's system command
+      % picks up stray keystrokes being written into
+      % the terminal but puts those at the beginning of
+      % what is returned by system. so we run the
+      % command twice and find the matching end part of
+      % the string to get the hostname
+      minlen = min(length(hostname), length(hostname2));
+      for k = 0:minlen
+        if (k < minlen)
+          if hostname(length(hostname) - k) ~= hostname2(length(hostname2) - k)
+            break
+          end
+        end
       end
-    end
-    if (k > 0)
-      hostname = hostname(length(hostname)-k+1:length(hostname));
-      hostname = lower(hostname);
-      hostname = hostname(find(((hostname <= 'z') & (hostname >= 'a')) | '.'));
+
+      if (k > 0)
+        hostname = hostname(length(hostname) - k + 1 : length(hostname));
+        hostname = lower(hostname);
+        hostname = hostname(find(((hostname <= 'z') & (hostname >= 'a')) | '.'));
+      else
+        hostname = 'unknown';
+      end
     else
       hostname = 'unknown';
     end
   else
     hostname = 'unknown';
   end
-else
-  hostname = 'unknown';
-end
 
+  % save to make it faster next time
+  globalHostname = hostname;
+else
+  hostname = globalHostname;
+end
