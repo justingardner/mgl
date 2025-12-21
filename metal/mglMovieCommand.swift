e6894a44be2fa78bcb569bf166b6d92000b902d8
Justin Gardner <jlg@stanford.edu>
2025-12-21 14:32:06 +0800

    Adding shibuya movie for testing and command for mglMovie

---
+++ metal/mglMovieCommand.swift
@@ -0,0 +1,14 @@
+//
+//  mglMovie.swift
+//  mglMetal
+//
+//  Created by Justin Gardner on 12/20/25.
+//  Copyright © 2025 GRU. All rights reserved.
+//
+
+import Foundation
+import MetalKit
+
+class mglMovie : mglCommand {
+}
+
