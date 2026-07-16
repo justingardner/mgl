ef5a303c25911c391ed39357023cb1a338de1141
Justin Gardner <jlg@stanford.edu>
2026-07-15 23:20:33 -0700

    Adding mglInfoCommand to return information including uuid on all connected motiors

--- metal/mglMetal/commands/mglInfoCommand.swift
+++ metal/mglMetal/commands/mglInfoCommand.swift
@@ -9,17 +9,27 @@
 import Foundation
 import MetalKit
 
+// structure for carrying info about displays
+struct mglDisplayInfo {
+    let displayID: CGDirectDisplayID
+    let uuid: String
+    let vendor: UInt32
+    let model: UInt32
+    let serialNumber: UInt32
+    var widthPixels: Int
+    var heightPixels: Int
+    var refreshRate: Double
+    var name: String = ""
+}
+
 class mglInfoCommand : mglCommand {
-    var device: MTLDevice!
-    var view: MTKView!
+    var device: MTLDevice?
+    var view: MTKView?
     
     // info about displays
-    var displayID: CGDirectDisplayID = 0
-    var displayUUID: String = ""
-    var displayVendor: UInt32 = 0
-    var displayModel: UInt32 = 0
-    var displaySerialNumber: UInt32 = 0
-
+    var currentDisplayIndex: Int = -1
+    var displays: [mglDisplayInfo] = []
+    
     override func doNondrawingWork(
         logger: mglLogger,
         view: MTKView,
@@ -37,12 +47,9 @@ class mglInfoCommand : mglCommand {
         self.device = device
         self.view = view
         
-        // Initialize display information in case we can't retrieve it.
-        displayID = 0
-        displayUUID = ""
-        displayVendor = 0
-        displayModel = 0
-        displaySerialNumber = 0
+        // reinitialize variables
+        currentDisplayIndex = -1
+        displays.removeAll()
 
         guard
             let window = view.window,
@@ -54,20 +61,63 @@ class mglInfoCommand : mglCommand {
             return true
         }
         
-        // get display info
-        displayID = CGDirectDisplayID(screenNumber.uint32Value)
-        displayVendor = CGDisplayVendorNumber(displayID)
-        displayModel = CGDisplayModelNumber(displayID)
-        displaySerialNumber = CGDisplaySerialNumber(displayID)
+        // Initialize display information in case we can't retrieve it.
+        let currentDisplayID = CGDirectDisplayID(screenNumber.uint32Value)
+        
+        // gather information about displays
+        for (index, screen) in NSScreen.screens.enumerated() {
+            
+            guard let screenNumber = screen.deviceDescription[
+                NSDeviceDescriptionKey("NSScreenNumber")
+            ] as? NSNumber else {
+                continue
+            }
 
-        if let uuid = CGDisplayCreateUUIDFromDisplayID(displayID)?.takeRetainedValue() {
-            displayUUID = CFUUIDCreateString(nil, uuid) as String
+            let id = CGDirectDisplayID(screenNumber.uint32Value)
+            
+            // if this is the index of the current display, then keep it
+            if id == currentDisplayID {
+                currentDisplayIndex = index
+            }
+
+            let uuid: String
+            if let cfUUID = CGDisplayCreateUUIDFromDisplayID(id)?.takeRetainedValue() {
+                uuid = CFUUIDCreateString(nil, cfUUID) as String
+            } else {
+                uuid = ""
+            }
+            // get refresh rate
+            var refreshRate = 0.0
+            if let mode = CGDisplayCopyDisplayMode(id) {
+                refreshRate = mode.refreshRate
+            }
+            // populate display info for this structure
+            var display = mglDisplayInfo(
+                displayID: id,
+                uuid: uuid,
+                vendor: CGDisplayVendorNumber(id),
+                model: CGDisplayModelNumber(id),
+                serialNumber: CGDisplaySerialNumber(id),
+                widthPixels: CGDisplayPixelsWide(id),
+                heightPixels: CGDisplayPixelsHigh(id),
+                refreshRate: refreshRate
+            )
+            
+            // add human readable display name
+            display.name = displayName(for: display)
+
+            displays.append(display)
         }
 
         return true
     }
 
     override func writeQueryResults(logger: mglLogger, commandInterface : mglCommandInterface) -> Bool {
+        // return false if we were unable to get device and view
+        guard let device, let view else {
+            return false
+        }
+        
         // send GPU name
         _ = commandInterface.writeCommand(data: mglSendString)
         _ = commandInterface.writeString(data: "gpu.name")
@@ -125,12 +175,12 @@ class mglInfoCommand : mglCommand {
         _ = commandInterface.writeString(data: "view.colorPixelFormatString")
         _ = commandInterface.writeCommand(data: mglSendString)
         switch view.colorPixelFormat {
-        case MTLPixelFormat.bgra8Unorm: _ = commandInterface.writeString(data: "bgra8Unorm")
-        case MTLPixelFormat.bgra8Unorm_srgb: _ = commandInterface.writeString(data: "bgra8Unorm_srgb")
-        case MTLPixelFormat.rgba16Float: _ = commandInterface.writeString(data: "rgba16Float")
-        case MTLPixelFormat.rgb10a2Unorm: _ = commandInterface.writeString(data: "rgb10a2Unorm")
-        case MTLPixelFormat.bgr10a2Unorm: _ = commandInterface.writeString(data: "bgr10a2Unorm")
-        default: _ = commandInterface.writeString(data: "Unknown")
+            case MTLPixelFormat.bgra8Unorm: _ = commandInterface.writeString(data: "bgra8Unorm")
+            case MTLPixelFormat.bgra8Unorm_srgb: _ = commandInterface.writeString(data: "bgra8Unorm_srgb")
+            case MTLPixelFormat.rgba16Float: _ = commandInterface.writeString(data: "rgba16Float")
+            case MTLPixelFormat.rgb10a2Unorm: _ = commandInterface.writeString(data: "rgb10a2Unorm")
+            case MTLPixelFormat.bgr10a2Unorm: _ = commandInterface.writeString(data: "bgr10a2Unorm")
+            default: _ = commandInterface.writeString(data: "Unknown")
         }
 
         _ = commandInterface.writeCommand(data: mglSendString)
@@ -145,30 +195,82 @@ class mglInfoCommand : mglCommand {
         let drawableSize: [Double] = [Double(view.drawableSize.width), Double(view.drawableSize.height)]
         _ = commandInterface.writeDoubleArray(data: drawableSize)
 
+        // send number of displays
+        _ = commandInterface.writeCommand(data: mglSendString)
+        _ = commandInterface.writeString(data: "display.numDisplays")
+        _ = commandInterface.writeCommand(data: mglSendDouble)
+        _ = commandInterface.writeDouble(data: Double(displays.count))
+
+        // send number of displays
+        _ = commandInterface.writeCommand(data: mglSendString)
+        _ = commandInterface.writeString(data: "display.currentDisplayIndex")
+        _ = commandInterface.writeCommand(data: mglSendDouble)
+        _ = commandInterface.writeDouble(data: Double(currentDisplayIndex))
+
         // send display information
-        _ = commandInterface.writeCommand(data: mglSendString)
-        _ = commandInterface.writeString(data: "display.uuid")
-        _ = commandInterface.writeCommand(data: mglSendString)
-        _ = commandInterface.writeString(data: displayUUID)
+        for (index, display) in displays.enumerated() {
 
-        _ = commandInterface.writeCommand(data: mglSendString)
-        _ = commandInterface.writeString(data: "display.vendor")
-        _ = commandInterface.writeCommand(data: mglSendDouble)
-        _ = commandInterface.writeDouble(data: Double(displayVendor))
-        
-        _ = commandInterface.writeCommand(data: mglSendString)
-        _ = commandInterface.writeString(data: "display.model")
-        _ = commandInterface.writeCommand(data: mglSendDouble)
-        _ = commandInterface.writeDouble(data: Double(displayModel))
-        
-        _ = commandInterface.writeCommand(data: mglSendString)
-        _ = commandInterface.writeString(data: "display.serialNumber")
-        _ = commandInterface.writeCommand(data: mglSendDouble)
-        _ = commandInterface.writeDouble(data: Double(displaySerialNumber))
+            let prefix = "display\(index)."
 
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "uuid")
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: display.uuid)
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "vendor")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: Double(display.vendor))
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "model")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: Double(display.model))
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "serialNumber")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: Double(display.serialNumber))
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "widthPixels")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: Double(display.widthPixels))
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "heightPixels")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: Double(display.heightPixels))
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "refreshRate")
+            _ = commandInterface.writeCommand(data: mglSendDouble)
+            _ = commandInterface.writeDouble(data: display.refreshRate)
+
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: prefix + "name")
+            _ = commandInterface.writeCommand(data: mglSendString)
+            _ = commandInterface.writeString(data: display.name)
+        }
         // send finished
         _ = commandInterface.writeCommand(data: mglSendFinished)
 
         return true
     }
 }
+
+/////////////////////////////////////////////////////////////////////
+// get human readable monitor device name
+/////////////////////////////////////////////////////////////////////
+func displayName(for displayInfo: mglDisplayInfo) -> String {
+
+    if let name = NSScreen.screens.first(where: {
+         ($0.deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")] as? NSNumber)?.uint32Value
+             == displayInfo.displayID
+     })?.localizedName {
+         return name
+     }
+
+     return "Unknown"
+}
+
